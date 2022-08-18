a98735ed6bfd61982459c3c2b75d62a0155560e1
barkstone <barkstone2@gmail.com>
2022-08-18 21:30:57 +0900

    프로젝트 생성

---
+++ src/main/java/todayquest/TodayquestApplication.java
@@ -0,0 +1,13 @@
+package todayquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayquestApplication.class, args);
+	}
+
+}

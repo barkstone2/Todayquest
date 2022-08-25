852731b606de9b620fc8614a961ea954ac8ae7ed
barkstone <barkstone2@gmail.com>
2022-08-25 18:05:03 +0900

    불필요한 import 제거

--- src/main/java/todayquest/quest/repository/QuestRepository.java
+++ src/main/java/todayquest/quest/repository/QuestRepository.java
@@ -1,7 +1,6 @@
 package todayquest.quest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 import todayquest.quest.entity.Quest;
 import todayquest.user.entity.UserInfo;

2080a598b8d8ecd74da143a85eb053a21bd868b9
barkstone <barkstone2@gmail.com>
2022-10-17 22:41:55 +0900

    Reward CRUD 기능 및 view 추가

---
+++ src/main/java/todayquest/reward/controller/RewardController.java
@@ -0,0 +1,58 @@
+package todayquest.reward.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import todayquest.reward.dto.RewardRequestDto;
+import todayquest.reward.entity.RewardGrade;
+import todayquest.reward.service.RewardService;
+import todayquest.user.dto.UserPrincipal;
+
+@RequiredArgsConstructor
+@RequestMapping("/rewards")
+@Controller
+public class RewardController {
+    private final RewardService rewardService;
+
+    @GetMapping("")
+    public String list(Model model, @AuthenticationPrincipal UserPrincipal principal) {
+        model.addAttribute("rewards", rewardService.getRewardList(principal));
+        return "reward/list";
+    }
+
+    @GetMapping("/{rewardId}")
+    public String view(@PathVariable("rewardId") Long rewardId, Model model, @AuthenticationPrincipal UserPrincipal principal) {
+        model.addAttribute("reward", rewardService.getReward(rewardId, principal));
+        model.addAttribute("gradeList", RewardGrade.getEnumList());
+        return "reward/view";
+    }
+
+    @GetMapping("/save")
+    public String saveForm(Model model) {
+        model.addAttribute("reward", new RewardRequestDto());
+        model.addAttribute("gradeList", RewardGrade.getEnumList());
+        return "reward/save";
+    }
+
+
+    @PostMapping("/save")
+    public String save(RewardRequestDto dto, @AuthenticationPrincipal UserPrincipal principal) {
+        rewardService.saveReward(dto, principal);
+        return "redirect:/rewards";
+    }
+
+    @PutMapping("/{rewardId}")
+    public String update(RewardRequestDto dto, @PathVariable("rewardId") Long rewardId, @AuthenticationPrincipal UserPrincipal principal) {
+        rewardService.updateReward(dto, rewardId, principal);
+        return "redirect:/rewards/{rewardId}";
+    }
+
+    @DeleteMapping("/{rewardId}")
+    public String delete(@PathVariable("rewardId") Long rewardId, @AuthenticationPrincipal UserPrincipal principal) {
+        rewardService.deleteReward(rewardId, principal);
+        return "redirect:/rewards";
+    }
+
+}
--- src/main/java/todayquest/user/dto/UserPrincipal.java
+++ src/main/java/todayquest/user/dto/UserPrincipal.java
@@ -3,7 +3,6 @@ package todayquest.user.dto;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;

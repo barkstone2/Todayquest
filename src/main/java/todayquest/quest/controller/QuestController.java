02d9758784cde5353eb5efd9a565c9d60a821d1d
barkstone <barkstone2@gmail.com>
2022-10-17 16:58:43 +0900

    퀘스트 수정, 삭제 시 유저 정보 확인 로직 추가

--- src/main/java/todayquest/quest/controller/QuestController.java
+++ src/main/java/todayquest/quest/controller/QuestController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import todayquest.quest.dto.QuestRequestDto;
 import todayquest.quest.entity.QuestDifficulty;
 import todayquest.quest.service.QuestService;
@@ -56,14 +57,23 @@ public class QuestController {
     }
 
     @PutMapping("/{questId}")
-    public String update(QuestRequestDto dto, @PathVariable("questId") Long questId) {
-        questService.updateQuest(dto, questId);
+    public String update(QuestRequestDto dto, @PathVariable("questId") Long questId, @AuthenticationPrincipal UserPrincipal principal, RedirectAttributes redirectAttributes) {
+        boolean isUpdated = questService.updateQuest(dto, questId, principal.getUserId());
+        if (!isUpdated) {
+            redirectAttributes.addAttribute("message", "비정상적인 접근입니다.");
+            return "redirect:/quests";
+        }
+
         return "redirect:/quests/{questId}";
     }
 
     @DeleteMapping("/{questId}")
-    public String delete(@PathVariable("questId") Long questId) {
-        questService.deleteQuest(questId);
+    public String delete(@PathVariable("questId") Long questId, @AuthenticationPrincipal UserPrincipal principal, RedirectAttributes redirectAttributes) {
+        boolean isDeleted = questService.deleteQuest(questId, principal.getUserId());
+        if (!isDeleted) {
+            redirectAttributes.addAttribute("message", "비정상적인 접근입니다.");
+        }
+
         return "redirect:/quests";
     }
 

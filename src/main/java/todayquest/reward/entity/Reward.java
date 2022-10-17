88a0d2607c43324fe300ae997385048bbac8a102
barkstone <barkstone2@gmail.com>
2022-10-17 10:52:15 +0900

    Reward 엔티티 추가

---
+++ src/main/java/todayquest/reward/entity/Reward.java
@@ -0,0 +1,36 @@
+package todayquest.reward.entity;
+
+import lombok.*;
+import todayquest.user.entity.UserInfo;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor @Builder
+@Entity
+public class Reward {
+
+    @Id @GeneratedValue
+    @Column(name = "reward_id")
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 100)
+    private String description;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private UserInfo user;
+
+    @Enumerated(EnumType.STRING)
+    private RewardGrade grade;
+
+    public void updateReward(String name) {
+        this.name = name;
+    }
+}

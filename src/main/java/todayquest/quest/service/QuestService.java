2e6c9738ae0badd3fd00f55c14b81dbc076a92c0
barkstone <barkstone2@gmail.com>
2022-10-17 11:04:48 +0900

    Quest 엔티티 등록, 수정 시 QuestReward 처리 로직 추가

--- src/main/java/todayquest/quest/service/QuestService.java
+++ src/main/java/todayquest/quest/service/QuestService.java
@@ -6,7 +6,11 @@ import org.springframework.transaction.annotation.Transactional;
 import todayquest.quest.dto.QuestRequestDto;
 import todayquest.quest.dto.QuestResponseDto;
 import todayquest.quest.entity.Quest;
+import todayquest.quest.entity.QuestReward;
 import todayquest.quest.repository.QuestRepository;
+import todayquest.quest.repository.QuestRewardRepository;
+import todayquest.reward.entity.Reward;
+import todayquest.reward.repository.RewardRepository;
 import todayquest.user.dto.UserPrincipal;
 import todayquest.user.entity.UserInfo;
 import todayquest.user.repository.UserRepository;
@@ -21,6 +25,9 @@ public class QuestService {
 
     private final QuestRepository questRepository;
     private final UserRepository userRepository;
+    private final RewardRepository rewardRepository;
+    private final QuestRewardRepository questRewardRepository;
+
 
     public List<QuestResponseDto> getQuestList(Long userId) {
         return questRepository.getQuestsByUserOrderByDeadLineDateAscDeadLineTimeAsc(UserInfo.builder().id(userId).build())
@@ -35,12 +42,20 @@ public class QuestService {
 
     public void saveQuest(QuestRequestDto dto, UserPrincipal principal) {
         UserInfo findId = userRepository.getById(principal.getUserId());
-        questRepository.save(dto.mapToEntity(findId));
+        Quest savedQuest = questRepository.save(dto.mapToEntity(findId));
+
+        List<Reward> rewards = rewardRepository.findAllById(dto.getRewards());
+        List<QuestReward> collect = rewards.stream()
+                .map(r -> QuestReward.builder().reward(r).quest(savedQuest).build())
+                .collect(Collectors.toList());
+        questRewardRepository.saveAll(collect);
     }
 
     public void updateQuest(QuestRequestDto dto, Long questId) {
         Quest findQuest = questRepository.getById(questId);
-        findQuest.updateQuestEntity(dto);
+        List<Reward> updateRewards = rewardRepository.findAllById(dto.getRewards());
+
+        findQuest.updateQuestEntity(dto, updateRewards);
     }
 
     public void deleteQuest(Long questId) {

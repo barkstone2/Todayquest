719cf0b77647298f0bbe12468860d84dd7369cce
barkstone <barkstone2@gmail.com>
2022-10-18 21:59:37 +0900

    ExceptionHandler에 AccessDeniedException 처리 로직 추가

--- src/main/java/todayquest/exception/CustomHandlerExceptionResolver.java
+++ src/main/java/todayquest/exception/CustomHandlerExceptionResolver.java
@@ -2,6 +2,7 @@ package todayquest.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +27,13 @@ public class CustomHandlerExceptionResolver implements HandlerExceptionResolver 
         } else if (ex instanceof IllegalStateException) {
             ModelAndView modelAndView = new ModelAndView("/oauth-login");
             modelAndView.setStatus(HttpStatus.UNAUTHORIZED);
+            ModelMap modelMap = modelAndView.getModelMap();
+            modelMap.put("message", ex.getMessage());
+
+            return modelAndView;
+        } else if (ex instanceof AccessDeniedException) {
+            ModelAndView modelAndView = new ModelAndView("/error/error");
+            modelAndView.setStatus(HttpStatus.FORBIDDEN);
             ModelMap modelMap = modelAndView.getModelMap();
             modelMap.put("message", ex.getMessage());
 

f962172f567419eaace0950276f04f4492912902
barkstone <barkstone2@gmail.com>
2022-10-18 17:32:44 +0900

    Reward Validation 추가

--- src/main/java/todayquest/reward/dto/RewardRequestDto.java
+++ src/main/java/todayquest/reward/dto/RewardRequestDto.java
@@ -1,31 +1,44 @@
 package todayquest.reward.dto;
 
+import com.mysema.commons.lang.Assert;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import todayquest.common.MessageUtil;
 import todayquest.reward.entity.Reward;
 import todayquest.reward.entity.RewardGrade;
 import todayquest.user.entity.UserInfo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class RewardRequestDto {
 
-    private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @NotNull
     private RewardGrade grade;
 
     public Reward mapToEntity(UserInfo userInfo) {
+
         return Reward.builder()
                 .user(userInfo)
                 .grade(this.grade)
                 .description(this.description)
                 .name(this.name)
                 .build();
+    }
+
+    @Builder
+    public RewardRequestDto(String name, String description, RewardGrade grade) {
+        this.name = name;
+        this.description = description;
+        this.grade = grade;
     }
 }

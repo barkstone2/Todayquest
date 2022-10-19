2f936c11cf5da38452fe117e301c979282d73734
barkstone <barkstone2@gmail.com>
2022-10-19 22:08:02 +0900

    퀘스트 상태 변경 기능 마무리, 퀘스트 완료시 아이템 획득 로직 추가

--- src/main/java/todayquest/quest/service/QuestService.java
+++ src/main/java/todayquest/quest/service/QuestService.java
@@ -5,10 +5,13 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import todayquest.common.MessageUtil;
+import todayquest.item.entity.Item;
+import todayquest.item.repository.ItemRepository;
 import todayquest.quest.dto.QuestRequestDto;
 import todayquest.quest.dto.QuestResponseDto;
 import todayquest.quest.entity.Quest;
 import todayquest.quest.entity.QuestReward;
+import todayquest.quest.entity.QuestState;
 import todayquest.quest.repository.QuestRepository;
 import todayquest.quest.repository.QuestRewardRepository;
 import todayquest.reward.entity.Reward;
@@ -28,6 +31,8 @@ public class QuestService {
     private final QuestRepository questRepository;
     private final UserRepository userRepository;
     private final RewardRepository rewardRepository;
+    private final ItemRepository itemRepository;
+
     private final QuestRewardRepository questRewardRepository;
 
     public List<QuestResponseDto> getQuestList(Long userId) {
@@ -56,11 +61,17 @@ public class QuestService {
 
     public void updateQuest(QuestRequestDto dto, Long questId, Long userId) {
         Optional<Quest> findQuest = questRepository.findById(questId);
-        Quest quest = findQuest.orElseThrow(() -> new IllegalArgumentException(MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest"))));
+        Quest quest = findQuest.orElseThrow(() -> new IllegalArgumentException(
+                MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest"))));
 
         if (quest.getUser().getId().equals(userId)) {
+            if (!quest.getState().equals(QuestState.PROCEED)) {
+                throw new IllegalArgumentException(MessageUtil.getMessage("quest.error.update.invalid.state"));
+            }
+
             List<Reward> updateRewards = rewardRepository.findAllById(dto.getRewards());
             quest.updateQuestEntity(dto, updateRewards);
+
         } else {
             throw new AccessDeniedException(MessageUtil.getMessage("exception.access.denied", MessageUtil.getMessage("quest")));
         }
@@ -68,9 +79,61 @@ public class QuestService {
 
     public void deleteQuest(Long questId, Long userId) {
         Optional<Quest> findQuest = questRepository.findById(questId);
-        Quest quest = findQuest.orElseThrow(() -> new IllegalArgumentException(MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest"))));
+        Quest quest = findQuest.orElseThrow(() -> new IllegalArgumentException(
+                MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest"))));
+
         if (quest.getUser().getId().equals(userId)) {
-            questRepository.deleteById(questId);
+            quest.changeState(QuestState.DELETE);
+        } else {
+            throw new AccessDeniedException(MessageUtil.getMessage("exception.access.denied", MessageUtil.getMessage("quest")));
+        }
+    }
+
+    public void completeQuest(Long questId, Long userId) {
+        Optional<Quest> findQuest = questRepository.findById(questId);
+        Quest quest = findQuest
+                .orElseThrow(() -> new IllegalArgumentException(
+                        MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest")))
+                );
+
+        UserInfo questOwner = quest.getUser();
+        if (questOwner.getId().equals(userId)) {
+            if(quest.getState().equals(QuestState.DELETE)) {
+                throw new IllegalArgumentException(MessageUtil.getMessage("quest.error.deleted"));
+            }
+
+            quest.changeState(QuestState.COMPLETE);
+
+            List<Reward> rewardList = quest.getRewards().stream()
+                    .map(qr -> qr.getReward()).collect(Collectors.toList());
+
+            List<Long> dirtyCheckIds = rewardList.stream()
+                    .map(r -> r.getId())
+                    .collect(Collectors.toList());
+            List<Item> dirtyCheckList = itemRepository.findAllByRewardIdsAndUserId(dirtyCheckIds, userId);
+            dirtyCheckList.stream().forEach(r -> r.addCount());
+
+            List<Item> saveList = rewardList.stream().filter(r -> !dirtyCheckIds.contains(r.getId()))
+                    .map(r -> Item.builder().reward(r).user(questOwner).build())
+                    .collect(Collectors.toList());
+
+            itemRepository.saveAll(saveList);
+        } else {
+            throw new AccessDeniedException(MessageUtil.getMessage("exception.access.denied", MessageUtil.getMessage("quest")));
+        }
+    }
+
+    public void discardQuest(Long questId, Long userId) {
+        Optional<Quest> findQuest = questRepository.findById(questId);
+        Quest quest = findQuest.orElseThrow(() -> new IllegalArgumentException(
+                MessageUtil.getMessage("exception.entity.notfound", MessageUtil.getMessage("quest"))));
+
+        if (quest.getUser().getId().equals(userId)) {
+            if(quest.getState().equals(QuestState.DELETE)) {
+                throw new IllegalArgumentException(MessageUtil.getMessage("quest.error.deleted"));
+            }
+
+            quest.changeState(QuestState.DISCARD);
         } else {
             throw new AccessDeniedException(MessageUtil.getMessage("exception.access.denied", MessageUtil.getMessage("quest")));
         }

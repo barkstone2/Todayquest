fadbe1465b94090055310f6022519525b112285d
barkstone <barkstone2@gmail.com>
2022-10-22 00:01:53 +0900

    인벤토리 기본 레이아웃 추가

--- src/main/java/todayquest/reward/entity/RewardGrade.java
+++ src/main/java/todayquest/reward/entity/RewardGrade.java
@@ -9,14 +9,14 @@ import java.util.stream.Collectors;
 @Getter
 public enum RewardGrade {
     F("F등급", "#9e9e9e"),
-    E("E등급", "#cccccc"),
+    E("E등급", "#c5fdb8"),
 
-    D("D등급", "#4ea3ed"),
-    C("C등급", "#a056eb"),
+    D("D등급", "#fcf7ba"),
+    C("C등급", "#3986ff"),
 
-    B("B등급", "#4ea3ed"),
-    A("A등급", ""),
-    S("S등급", "");
+    B("B등급", "#a056eb"),
+    A("A등급", "#fea552"),
+    S("S등급", "#f23f3f");
 
     String text;
     String color;

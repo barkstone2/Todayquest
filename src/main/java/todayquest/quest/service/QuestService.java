b77b9d54e828ff467bd503d339e6bbf705dc9af2
barkstone <barkstone2@gmail.com>
2022-10-22 12:49:48 +0900

    퀘스트 완료 시 경험치, 골드 획득 및 레벨업 체크 로직 추가

--- src/main/java/todayquest/quest/service/QuestService.java
+++ src/main/java/todayquest/quest/service/QuestService.java
@@ -1,5 +1,7 @@
 package todayquest.quest.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -21,10 +23,13 @@ import todayquest.quest.repository.QuestRepository;
 import todayquest.quest.repository.QuestRewardRepository;
 import todayquest.reward.entity.Reward;
 import todayquest.reward.repository.RewardRepository;
+import todayquest.user.dto.UserPrincipal;
 import todayquest.user.entity.UserInfo;
 import todayquest.user.repository.UserRepository;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -39,8 +44,8 @@ public class QuestService {
     private final ItemRepository itemRepository;
 
     private final QuestRewardRepository questRewardRepository;
+    private final ResourceLoader resourceLoader;
 
-    @Transactional
     public Slice<QuestResponseDto> getQuestList(Long userId, QuestState state, Pageable pageable) {
 
         PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
@@ -99,7 +104,13 @@ public class QuestService {
         }
     }
 
-    public void completeQuest(Long questId, Long userId) {
+    public void completeQuest(Long questId, UserPrincipal principal) throws IOException {
+        // 경험치 테이블을 읽어온다.
+        Resource resource = resourceLoader.getResource("classpath:data/exp_table.json");
+        ObjectMapper om = new ObjectMapper();
+        Map<Integer, Long> expTable = om.readValue(resource.getInputStream(), new TypeReference<>() {});
+        Long targetExp = expTable.get(principal.getLevel());
+
         Optional<Quest> findQuest = questRepository.findById(questId);
         Quest quest = findQuest
                 .orElseThrow(() -> new IllegalArgumentException(
@@ -107,27 +118,47 @@ public class QuestService {
                 );
 
         UserInfo questOwner = quest.getUser();
-        if (questOwner.getId().equals(userId)) {
+        if (questOwner.getId().equals(principal.getUserId())) {
             if(quest.getState().equals(QuestState.DELETE)) {
                 throw new IllegalArgumentException(MessageUtil.getMessage("quest.error.deleted"));
             }
 
-            quest.changeState(QuestState.COMPLETE);
-
+            // 퀘스트에 등록된 Reward 정보를 가져온다.
             List<Reward> rewardList = quest.getRewards().stream()
-                    .map(qr -> qr.getReward()).collect(Collectors.toList());
+                    .map(qr -> qr.getReward())
+                    .collect(Collectors.toList());
 
-            List<Long> dirtyCheckIds = rewardList.stream()
+            // Reward의 ID 값만 가져온다.
+            List<Long> rewardIds = rewardList.stream()
                     .map(r -> r.getId())
                     .collect(Collectors.toList());
-            List<Item> dirtyCheckList = itemRepository.findAllByRewardIdsAndUserId(dirtyCheckIds, userId);
-            dirtyCheckList.stream().forEach(r -> r.addCount());
 
-            List<Item> saveList = rewardList.stream().filter(r -> !dirtyCheckIds.contains(r.getId()))
-                    .map(r -> Item.builder().reward(r).user(questOwner).build())
+            // 퀘스트 보상 아이템 중 이미 인벤토리에 있는 아이템을 찾아 dirty checking으로 개수를 증가시킨다.
+            List<Item> dirtyCheckItemList = itemRepository.findAllByRewardIdsAndUserId(rewardIds, principal.getUserId());
+            dirtyCheckItemList.stream().forEach(r -> r.addCount());
+
+            // 더티 체킹으로 업데이트한 아이템의 Reward ID 값만 가져온다.
+            List<Long> dirtyCheckIds = dirtyCheckItemList.stream()
+                    .map(i -> i.getReward().getId())
                     .collect(Collectors.toList());
 
+            // 퀘스트의 보상 아이템 중 더티체킹으로 업데이트 하지 않은, 즉 새로 등록할 아이템을 걸러내 새로 등록한다.
+            List<Item> saveList = rewardList.stream()
+                    .filter(r -> !dirtyCheckIds.contains(r.getId()))
+                    .map(r -> Item.builder().reward(r).user(questOwner).build())
+                    .collect(Collectors.toList());
             itemRepository.saveAll(saveList);
+
+            // 퀘스트의 상태를 완료 상태로 변경한다.
+            quest.changeState(QuestState.COMPLETE);
+
+            // 더티체킹으로 퀘스트를 클리어한 유저의 경험치와 골드를 증가시킨다.
+            // 레벨업이 가능한 경우 레벨업을 시킨다.
+            questOwner.earnExpAndGold(quest.getDifficulty().getExperience(), quest.getDifficulty().getGold());
+            questOwner.levelUpCheck(targetExp);
+
+            // 로그인된 사용자의 정보를 동기화한다.
+            principal.synchronizeUserInfo(questOwner.getLevel(), questOwner.getExp(), questOwner.getGold());
         } else {
             throw new AccessDeniedException(MessageUtil.getMessage("exception.access.denied", MessageUtil.getMessage("quest")));
         }

b9815d8ac972a9388049d4ff05a74d11aaf7e941
barkstone <barkstone2@gmail.com>
2022-10-24 16:09:06 +0900

    DTO validation 추가 및 message 추가

--- src/main/java/todayquest/user/dto/UserRequestDto.java
+++ src/main/java/todayquest/user/dto/UserRequestDto.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.Size;
 @Data
 public class UserRequestDto {
 
-    @Pattern(regexp = "^([a-zA-Z0-9가-힣].)[a-zA-Z0-9가-힣\\s]*$", message = "{nickname.pattern}")
+    /** 영어 대소문자, 숫자, 한글 허용 & 한글 자모 비허용 & 시작과 끝을 제외한 위치에 공백 허용 */
+    @Pattern(regexp = "^([a-zA-Z0-9가-힣[^\\sㄱ-ㅎㅏ-ㅡ]]+)([a-zA-Z0-9가-힣\\s]*)([a-zA-Z0-9가-힣[^\\sㄱ-ㅎㅏ-ㅡ]]+)$", message = "{nickname.pattern}")
     @Size(max = 20, message = "{nickname.size}")
     private String nickname;
 }

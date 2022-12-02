3e7295cee6c91c927064f4b55165f123097f6b61
barkstone <barkstone2@gmail.com>
2022-12-02 22:06:03 +0900

    업적 서비스 공통 로직 분리 및 아이템 업적 달성 확인 로직 추가

--- src/main/java/todayquest/achievement/service/AchievementService.java
+++ src/main/java/todayquest/achievement/service/AchievementService.java
@@ -3,9 +3,11 @@ package todayquest.achievement.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import todayquest.achievement.entity.Achievement;
+import todayquest.achievement.entity.AchievementType;
 import todayquest.achievement.entity.UserAchievement;
 import todayquest.achievement.repository.AchievementRepository;
 import todayquest.achievement.repository.UserAchievementRepository;
+import todayquest.item.repository.ItemLogRepository;
 import todayquest.quest.repository.QuestLogRepository;
 import todayquest.user.entity.UserInfo;
 import todayquest.user.repository.UserRepository;
@@ -22,23 +24,34 @@ public class AchievementService {
     private final UserAchievementRepository userAchievementRepository;
     private final UserRepository userRepository;
     private final QuestLogRepository questLogRepository;
+    private final ItemLogRepository itemLogRepository;
 
     public void checkAndAttainQuestAchievement(Long userId) {
-        List<Achievement> achievements = achievementRepository.findAll();
+        Map<String, Long> analytics = questLogRepository.getQuestAnalytics(userId);
+        checkAndAttainAchievement(userId, AchievementType.QUEST, analytics);
+    }
+
+    public void checkAndAttainItemAchievement(Long userId) {
+        Map<String, Long> analytics = itemLogRepository.getItemAnalytics(userId);
+        checkAndAttainAchievement(userId, AchievementType.ITEM, analytics);
+    }
+
+    private void checkAndAttainAchievement(Long userId, AchievementType type, Map<String, Long> analytics) {
         List<UserAchievement> userAchievements = userAchievementRepository.findByUserId(userId);
-        Map<String, Long> countGroupByState = questLogRepository.getQuestAnalytics(userId);
-        UserInfo userInfo = userRepository.getById(userId);
 
         List<Long> attainedAchievementIds = userAchievements.stream()
                 .map(UserAchievement::getAchievementId)
                 .collect(Collectors.toList());
+
+        List<Achievement> achievements = achievementRepository.findByType(type);
+        UserInfo userInfo = userRepository.getById(userId);
 
         List<Achievement> notAttainedAchievements = achievements.stream()
                 .filter(achievement -> !attainedAchievementIds.contains(achievement.getId()))
                 .collect(Collectors.toList());
 
         List<UserAchievement> newlyAttainedAchievements = notAttainedAchievements.stream()
-                .filter(achievement -> achievement.getTargetNumber() <= countGroupByState.get(achievement.getAction().name()))
+                .filter(achievement -> achievement.getTargetNumber() <= analytics.get(achievement.getAction().name()))
                 .map(achievement -> new UserAchievement(userInfo, achievement))
                 .collect(Collectors.toList());
 

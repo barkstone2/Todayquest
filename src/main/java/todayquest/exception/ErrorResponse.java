0c9f34611a721addd899d75d173349976b86e712
barkstone <barkstone2@gmail.com>
2023-02-16 12:51:47 +0900

    feat: deserialize 문제로 MultiValueMap concrete 타입으로 변경

--- src/main/java/todayquest/exception/ErrorResponse.java
+++ src/main/java/todayquest/exception/ErrorResponse.java
@@ -5,14 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ErrorResponse {
     private String message;
     private HttpStatus status;
-    private final MultiValueMap<String, String> errors = new LinkedMultiValueMap<>();
+    private final LinkedMultiValueMap<String, String> errors = new LinkedMultiValueMap<>();
 
     public ErrorResponse(String message, HttpStatus status) {
         this.message = message;

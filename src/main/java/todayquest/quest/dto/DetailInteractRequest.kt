6da8124502cda732c54bbee398dcc069cbd5f5ea
barkstone <barkstone2@gmail.com>
2023-02-16 12:50:44 +0900

    feat: interact dto validation 추가

--- src/main/java/todayquest/quest/dto/DetailInteractRequest.kt
+++ src/main/java/todayquest/quest/dto/DetailInteractRequest.kt
@@ -1,7 +1,10 @@
 package todayquest.quest.dto
 
+import org.hibernate.validator.constraints.Range
+
 class DetailInteractRequest(
     count: Short? = null
 ){
+    @Range(min = 1, max = 255, message = "{Range.details.count}")
     var count = count ?: 1
 }

3a9d956952fdafe9b94fc2c14a209cb5e075a99b
barkstone <barkstone2@gmail.com>
2023-02-16 13:20:07 +0900

    feat: oauth2 login 관련 로직 제거

--- src/main/java/todayquest/user/service/UserService.java
+++ src/main/java/todayquest/user/service/UserService.java
@@ -6,17 +6,12 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import todayquest.common.MessageUtil;
 import todayquest.quest.entity.QuestType;
 import todayquest.user.dto.UserPrincipal;
 import todayquest.user.dto.UserRequestDto;
-import todayquest.user.entity.ProviderType;
 import todayquest.user.entity.UserInfo;
 import todayquest.user.repository.UserRepository;
 
@@ -32,47 +27,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final EntityManager em;
     private final ResourceLoader resourceLoader;
-
-    public UserPrincipal processUserInfo(OidcUserRequest request, OidcUser user) {
-        return processUserInfo((OAuth2UserRequest) request, user);
-    }
-
-    public UserPrincipal processUserInfo(OAuth2UserRequest request, OAuth2User user) {
-        ProviderType providerType = ProviderType.valueOf(request.getClientRegistration().getRegistrationId().toUpperCase());
-
-        // Oauth2 Provider 가 제공하는 ID 값 추출
-        String id = user.getName();
-        if(providerType.name().equals("NAVER")) {
-            Map<String, Object> info = user.getAttribute("response");
-            id = (String) info.get("id");
-        }
-
-        // 해당 Provider ID로 가입된 회원이 있나 조회
-        UserInfo savedUserInfo = userRepository.findByOauth2Id(id);
-
-        // 가입 이력이 없으면 유저 정보를 DB에 등록
-        if(savedUserInfo == null) {
-            String tempNickName = createRandomNickname();
-
-            while (isDuplicateNickname(tempNickName)) {
-                tempNickName = createRandomNickname();
-            }
-
-            UserInfo newUserInfo = new UserInfo(id, tempNickName, providerType);
-
-            // Dynamic Insert로 처리한 컬럼의 default 값을 반환하지 않음
-            UserInfo savedUser = userRepository.saveAndFlush(newUserInfo);
-
-            // 영속성 컨텍스트에 저장한 엔티티를 준영속 상태로 만듬
-            em.detach(savedUser);
-
-            // 별도의 조회 쿼리로 엔티티 교체
-            savedUserInfo = userRepository.getById(savedUser.getId());
-        }
-
-        // 유저 엔티티의 정보로 Principal을 만들어 반환
-        return UserPrincipal.create(savedUserInfo);
-    }
 
     public UserPrincipal getUserInfoById(Long id) {
         UserInfo userInfo = userRepository.getReferenceById(id);

68e6ef245aa4f32ea292ba41599270998849a798
barkstone <barkstone2@gmail.com>
2023-02-18 21:15:33 +0900

    feat: 세부 퀘스트 로직 퀘스트 엔티티 내부로 이동
    Cascade 를 통해 엔티티가 관리되도록 변경

--- src/main/java/todayquest/quest/repository/DetailQuestRepository.java
+++
@@ -1,7 +0,0 @@
-package todayquest.quest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import todayquest.quest.entity.DetailQuest;
-
-public interface DetailQuestRepository extends JpaRepository<DetailQuest, Long> {
-}

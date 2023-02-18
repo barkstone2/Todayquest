136ac070e3219665120077505aa8bedd1991cd40
barkstone <barkstone2@gmail.com>
2023-02-18 11:46:57 +0900

    feat: 레디스 Serializer 추가, value 타입 변경
    레드스로 경험치 테이블, 닉네임 prefix, postfix 분리를 위해 Serializer 추가 및 value generic String으로 변경

--- src/main/java/todayquest/config/RedisConfig.java
+++ src/main/java/todayquest/config/RedisConfig.java
@@ -5,15 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableRedisRepositories
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Integer.class));
+        redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Long.class));
         return redisTemplate;
     }
+
 }

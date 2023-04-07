a96af9522a67198a500e756190b2c7834d7bd9ba
barkstone <barkstone2@gmail.com>
2023-04-07 23:25:31 +0900

    feat: 유저 레벨 계산을 통해 반환하도록 변경

--- src/main/java/todayquest/user/service/UserService.java
+++ src/main/java/todayquest/user/service/UserService.java
@@ -8,12 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 import todayquest.common.MessageUtil;
 import todayquest.exception.DuplicateNicknameException;
 import todayquest.exception.RedisDataNotFoundException;
+import todayquest.properties.RedisKeyProperties;
 import todayquest.quest.entity.QuestType;
 import todayquest.user.dto.UserPrincipal;
 import todayquest.user.dto.UserRequestDto;
+import todayquest.user.entity.ProviderType;
 import todayquest.user.entity.UserInfo;
 import todayquest.user.repository.UserRepository;
 
+import java.util.Map;
 import java.util.Random;
 
 @Transactional
@@ -24,11 +27,38 @@ public class UserService {
     private final UserRepository userRepository;
     private final EntityManager em;
     private final RedisTemplate<String, String> redisTemplate;
+    private final RedisKeyProperties redisKeyProperties;
+
+    public UserPrincipal getOrRegisterUser(String oauth2Id, ProviderType providerType) {
+        UserInfo savedUserInfo = userRepository.findByOauth2Id(oauth2Id);
+
+        // 가입 이력이 없으면 유저 정보를 DB에 등록
+        if (savedUserInfo == null) {
+            String tempNickName = createRandomNickname();
+            while (isDuplicateNickname(tempNickName)) {
+                tempNickName = createRandomNickname();
+            }
+            UserInfo newUserInfo = new UserInfo(oauth2Id, tempNickName, providerType);
+
+            // Dynamic Insert로 처리한 컬럼의 default 값을 반환하지 않음
+            UserInfo savedUser = userRepository.saveAndFlush(newUserInfo);
+            em.detach(savedUser);
+
+            savedUserInfo = userRepository.getReferenceById(savedUser.getId());
+        }
+
+        Map<String, Long> expTable = redisTemplate.<String, Long>opsForHash().entries(redisKeyProperties.getExpTable());
+
+        return UserPrincipal.create(savedUserInfo, expTable);
+    }
 
     public UserPrincipal getUserInfoById(Long id) {
         UserInfo userInfo = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException(MessageUtil.getMessage("exception.badRequest")));
-        return UserPrincipal.create(userInfo);
+
+        Map<String, Long> expTable = redisTemplate.<String, Long>opsForHash().entries(redisKeyProperties.getExpTable());
+
+        return UserPrincipal.create(userInfo, expTable);
     }
 
     public boolean isDuplicateNickname(String nickname) {
@@ -47,11 +77,9 @@ public class UserService {
             }
 
             findUser.updateNickname(nicknameTrim);
-            principal.setNickname(nicknameTrim);
         }
 
         findUser.changeUserSettings(dto);
-        principal.changeUserSettings(dto);
     }
 
     public void earnExpAndGold(QuestType type, UserInfo user) {

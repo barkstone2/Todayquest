8f513e6d3efda6c68c1ed8141a9ca20881c8804b
barkstone <barkstone2@gmail.com>
2023-04-29 23:11:40 +0900

    test: 테스트 용 유저 정보 처리 클래스 수정

--- src/test/java/todayquest/common/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/todayquest/common/WithMockCustomUserSecurityContextFactory.java
@@ -7,7 +7,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 import todayquest.annotation.WithCustomMockUser;
 import todayquest.user.dto.UserPrincipal;
+import todayquest.user.entity.ProviderType;
 import todayquest.user.entity.UserInfo;
+
+import java.util.HashMap;
 
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
 
@@ -15,16 +18,9 @@ public class WithMockCustomUserSecurityContextFactory implements WithSecurityCon
     public SecurityContext createSecurityContext(WithCustomMockUser user) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-        UserPrincipal principal = UserPrincipal.create(
-                UserInfo.builder()
-                        .id(user.userId())
-                        .providerType(user.providerType())
-                        .nickname(user.nickname())
-                        .level(1)
-                        .gold(0L)
-                        .exp(0L)
-                        .build()
-        );
+        UserInfo userInfo = new UserInfo("", "", ProviderType.GOOGLE);
+
+        UserPrincipal principal = UserPrincipal.create(userInfo, new HashMap<>());
 
         Authentication auth = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
         context.setAuthentication(auth);

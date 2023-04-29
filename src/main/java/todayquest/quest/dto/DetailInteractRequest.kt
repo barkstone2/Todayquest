8730e0026e12494c85a29d487a9691a8d12ebc65
barkstone <barkstone2@gmail.com>
2023-04-29 22:56:19 +0900

    feat: 상세 퀘스트 상호작용 validation 조건 변경

--- src/main/java/todayquest/quest/dto/DetailInteractRequest.kt
+++ src/main/java/todayquest/quest/dto/DetailInteractRequest.kt
@@ -5,6 +5,6 @@ import org.hibernate.validator.constraints.Range
 class DetailInteractRequest(
     count: Int? = null
 ){
-    @Range(min = 1, max = 255, message = "{Range.details.count}")
+    @Range(min = 0, max = 255, message = "{Range.details.count}")
     var count = count ?: 1
 }

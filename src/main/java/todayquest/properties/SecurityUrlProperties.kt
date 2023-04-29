14580a2820570423992f270beb17839aca54f3e8
barkstone <barkstone2@gmail.com>
2023-04-29 21:14:59 +0900

    feat: 허용 URL 처리를 위한 컴포넌트 추가

---
+++ src/main/java/todayquest/properties/SecurityUrlProperties.kt
@@ -0,0 +1,11 @@
+package todayquest.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "security.url")
+class SecurityUrlProperties {
+    var allowedUrl: Array<String> = arrayOf()
+    var adminUrl: Array<String> = arrayOf()
+}

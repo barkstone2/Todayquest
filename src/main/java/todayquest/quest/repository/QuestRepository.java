1e142f2407c1fe0f99e31255b0475ef7f82bc088
barkstone <barkstone2@gmail.com>
2023-05-04 11:06:10 +0900

    remove: 배치 로직 제거
    배치 로직 별도의 애플리케이션으로 분리

--- src/main/java/todayquest/quest/repository/QuestRepository.java
+++ src/main/java/todayquest/quest/repository/QuestRepository.java
@@ -9,18 +9,9 @@ import org.springframework.stereotype.Repository;
 import todayquest.quest.entity.Quest;
 import todayquest.quest.entity.QuestState;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
 @Repository
 public interface QuestRepository extends JpaRepository<Quest, Long>, QuestRepositoryCustom {
     @Query("select q from Quest q where q.user.id = :userId and q.state= :state")
     Page<Quest> getQuestsList(@Param("userId") Long userId, @Param("state") QuestState state, Pageable pageable);
-
-    @Query("select q from Quest q where q.state = :state and q.deadLine = null and q.user.resetTime = :resetTime")
-    Page<Quest> getQuestsForResetBatch(@Param("state") QuestState state, @Param("resetTime") LocalTime resetTime, Pageable pageable);
-
-    @Query("select q from Quest q where q.state = :state and q.deadLine != null and q.deadLine <= :targetDate")
-    Page<Quest> getQuestForDeadLineBatch(@Param("state") QuestState state, @Param("targetDate") LocalDateTime targetDate, Pageable pageable);
 
 }

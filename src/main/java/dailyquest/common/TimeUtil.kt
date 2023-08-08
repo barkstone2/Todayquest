e3b688e96d2902b25ab450c3e28f6e7c91055b35
barkstone <barkstone2@gmail.com>
2023-08-08 21:21:33 +0900

    feat: 날짜 범위 계산 로직 수정
    분기 구분 시 13주 단위로 구분하도록 변경
    주의 시작일이 분기 범위를 벗어날 때도 처리
    날짜 범위 계산 테스트 코드 추가

--- src/main/java/dailyquest/common/TimeUtil.kt
+++ src/main/java/dailyquest/common/TimeUtil.kt
@@ -2,26 +2,31 @@ package dailyquest.common
 
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.temporal.TemporalAdjusters
 
 
-fun LocalDate.firstDayOfQuarter() : LocalDateTime {
-    val quarter = (this.monthValue - 1) / 3 + 1
-    val startMonth = (quarter - 1) * 3 + 1
-    val startOfQuarter = this.withMonth(startMonth).withDayOfMonth(1)
+fun LocalDate.firstDayOfQuarter(): LocalDate {
+    val week = (this.dayOfYear-1) / 7
+    val quarter = week / 13
+    val weeks = quarter * 13L
+    val startWeek = this.withMonth(1).withDayOfMonth(1).plusWeeks(weeks)
 
-    return startOfQuarter.atTime(0, 0, 0)
+    return startWeek.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
 }
 
-fun LocalDate.lastDayOfQuarter() : LocalDateTime {
-    val quarter = (this.monthValue - 1) / 3 + 1
-    val endMonth = quarter * 3
-    val endOfQuarter = this.withMonth(endMonth).withDayOfMonth(1).plusMonths(1).minusDays(1)
+fun LocalDate.lastDayOfQuarter(): LocalDate {
+    val week = (this.dayOfYear-1) / 7
+    val quarter = week / 13
+    val weeks = (quarter + 1) * 13L
+    val endWeek = this.withMonth(1).withDayOfMonth(1).plusWeeks(weeks -1)
 
-    return endOfQuarter.atTime(23, 59, 59)
+    return endWeek.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
 }
 
-fun LocalDateTime.firstDayOfWeek() : LocalDate {
+fun LocalDate.firstDayOfWeek() : LocalDate {
     return LocalDate.from(this.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)))
+}
+
+fun LocalDate.firstDayOfMonth() : LocalDate {
+    return LocalDate.from(this.with(TemporalAdjusters.firstDayOfMonth()))
 }

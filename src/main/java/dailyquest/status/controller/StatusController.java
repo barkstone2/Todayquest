cc953852640b0d44026a118d0b9689869a3dbf05
barkstone <barkstone2@gmail.com>
2023-08-11 16:48:45 +0900

    feat: 통계 조회 엔드 포인트 추가
    통계 응답 DTO 추가
    REST 응답 DTO에 정적 팩토리 메서드 추가

---
+++ src/main/java/dailyquest/status/controller/StatusController.java
@@ -0,0 +1,42 @@
+package dailyquest.status.controller;
+
+import dailyquest.common.ResponseData;
+import dailyquest.quest.dto.QuestLogSearchCondition;
+import dailyquest.quest.dto.QuestStatisticsResponse;
+import dailyquest.quest.service.QuestLogService;
+import dailyquest.status.dto.StatusResponse;
+import dailyquest.user.dto.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Validated
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/status")
+@RestController
+public class StatusController {
+
+    private final QuestLogService questLogService;
+
+    @GetMapping
+    public ResponseEntity<ResponseData<StatusResponse>> getStatus(
+            @RequestBody QuestLogSearchCondition questLogSearchCondition,
+            @AuthenticationPrincipal UserPrincipal principal
+    ) {
+        Map<LocalDate, QuestStatisticsResponse> questStatistic = questLogService.getQuestStatistic(principal.getId(), questLogSearchCondition);
+
+        StatusResponse statusResponse = new StatusResponse(questStatistic);
+
+        return ResponseEntity.ok(ResponseData.of(statusResponse));
+    }
+}

7e6c941709f2afebe4d992b8f239c7763997ae96
barkstone <barkstone2@gmail.com>
2023-08-11 16:48:45 +0900

    feat: 통계 조회 엔드 포인트 추가
    통계 응답 DTO 추가
    REST 응답 DTO에 정적 팩토리 메서드 추가

---
+++ src/main/java/dailyquest/status/dto/StatusResponse.java
@@ -0,0 +1,10 @@
+package dailyquest.status.dto;
+
+import dailyquest.quest.dto.QuestStatisticsResponse;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public record StatusResponse(
+    Map<LocalDate, QuestStatisticsResponse> questStatistics
+) {}

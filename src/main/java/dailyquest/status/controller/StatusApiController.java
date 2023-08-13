f1d307e76e68b199208cac51af70c3b5f5a076ca
barkstone <barkstone2@gmail.com>
2023-08-13 23:05:11 +0900

    feat: path variable 과 model attribute 사용하도록 변경
    상태창 컨트롤러 이름에 api 컨트롤러임을 표시
    RequestBody 제거 후 path variable과 함께 파라미터를 model attribute 로 처리
    상태창 컨틀롤러 테스트 코드 추가

--- src/main/java/dailyquest/status/controller/StatusApiController.java
+++ src/main/java/dailyquest/status/controller/StatusApiController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,13 +23,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/status")
 @RestController
-public class StatusController {
+public class StatusApiController {
 
     private final QuestLogService questLogService;
 
-    @GetMapping
+    @GetMapping("/{selectedDate}")
     public ResponseEntity<ResponseData<StatusResponse>> getStatus(
-            @RequestBody QuestLogSearchCondition questLogSearchCondition,
+            QuestLogSearchCondition questLogSearchCondition,
             @AuthenticationPrincipal UserPrincipal principal
     ) {
         Map<LocalDate, QuestStatisticsResponse> questStatistic = questLogService.getQuestStatistic(principal.getId(), questLogSearchCondition);

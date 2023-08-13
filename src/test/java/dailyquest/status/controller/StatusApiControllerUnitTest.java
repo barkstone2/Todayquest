025c84d098f9ca1fb69ba4499b9a6c238080f0b6
barkstone <barkstone2@gmail.com>
2023-08-13 23:05:11 +0900

    feat: path variable 과 model attribute 사용하도록 변경
    상태창 컨트롤러 이름에 api 컨트롤러임을 표시
    RequestBody 제거 후 path variable과 함께 파라미터를 model attribute 로 처리
    상태창 컨틀롤러 테스트 코드 추가

---
+++ src/test/java/dailyquest/status/controller/StatusApiControllerUnitTest.java
@@ -0,0 +1,87 @@
+package dailyquest.status.controller;
+
+import dailyquest.annotation.WithCustomMockUser;
+import dailyquest.config.SecurityConfig;
+import dailyquest.jwt.JwtAuthorizationFilter;
+import dailyquest.quest.dto.QuestLogSearchCondition;
+import dailyquest.quest.dto.QuestLogSearchType;
+import dailyquest.quest.service.QuestLogService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = StatusApiController.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityConfig.class, JwtAuthorizationFilter.class})})
+@WithCustomMockUser
+@DisplayName("상태창 API 컨트롤러 유닛 테스트")
+public class StatusApiControllerUnitTest {
+    static final String URI_PREFIX = "/api/v1/status";
+    
+    @Autowired
+    MockMvc mvc;
+    
+    @MockBean
+    QuestLogService questLogService;
+
+    @DisplayName("상태창 통계 요청 시 요청 파라미터를 사용해 데이터를 조회한다")
+    @Test
+    public void testGetStatus() throws Exception {
+        //given
+        LocalDate selectedDate = LocalDate.of(2022, 12, 12);
+        QuestLogSearchType searchType = QuestLogSearchType.WEEKLY;
+
+        String url = URI_PREFIX + "/" + selectedDate + "?searchType=" + searchType;
+
+        ArgumentCaptor<QuestLogSearchCondition> conditionCaptor = ArgumentCaptor.forClass(QuestLogSearchCondition.class);
+
+        //when
+        ResultActions result = mvc.perform(get(url));
+
+        //then
+        verify(questLogService).getQuestStatistic(any(), conditionCaptor.capture());
+        QuestLogSearchCondition requestCondition = conditionCaptor.getValue();
+        assertThat(requestCondition.getSearchType()).isEqualTo(searchType);
+        assertThat(requestCondition.getSelectedDate()).isEqualTo(selectedDate);
+        result.andExpect(status().isOk());
+    }
+
+    @DisplayName("상태창 통계 요청 시 search type 파라미터가 비어있다면 기본값을 사용한다")
+    @Test
+    public void testIfSelectedDateIsEmpty() throws Exception {
+        //given
+        LocalDate selectedDate = LocalDate.of(2022, 12, 12);
+        String url = URI_PREFIX + "/" + selectedDate;
+
+        ArgumentCaptor<QuestLogSearchCondition> conditionCaptor = ArgumentCaptor.forClass(QuestLogSearchCondition.class);
+        QuestLogSearchType defaultSearchType = new QuestLogSearchCondition().getSearchType();
+
+        //when
+        ResultActions result = mvc.perform(get(url));
+
+        //then
+        verify(questLogService).getQuestStatistic(any(), conditionCaptor.capture());
+        QuestLogSearchCondition requestCondition = conditionCaptor.getValue();
+        assertThat(requestCondition.getSelectedDate()).isEqualTo(selectedDate);
+        assertThat(requestCondition.getSearchType()).isEqualTo(defaultSearchType);
+        result.andExpect(status().isOk());
+    }
+
+}

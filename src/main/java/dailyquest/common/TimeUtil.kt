d5a31aed9e416d2fbf69021fec6d13f345b8993b
barkstone <barkstone2@gmail.com>
2023-08-18 14:28:36 +0900

    fix: 분기 시작일, 종료일이 제대로 처리되지 않는 버그 수정
    요청 날짜 연도의 첫 월요일을 기준으로 분기 계산하도록 수정
    코드 수정에 따라 테스트 코드도 수정, 보완

--- src/main/java/dailyquest/common/TimeUtil.kt
+++ src/main/java/dailyquest/common/TimeUtil.kt
@@ -2,25 +2,41 @@ package dailyquest.common
 
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.temporal.TemporalAdjuster
 import java.time.temporal.TemporalAdjusters
 
+private val firstMondayOfYearAdjusters: TemporalAdjuster = TemporalAdjusters.dayOfWeekInMonth(1, DayOfWeek.MONDAY)
 
 fun LocalDate.firstDayOfQuarter(): LocalDate {
-    val week = (this.dayOfYear-1) / 7
-    val quarter = week / 13
-    val weeks = quarter * 13L
-    val startWeek = this.withMonth(1).withDayOfMonth(1).plusWeeks(weeks)
 
-    return startWeek.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+    val firstMondayOfYear = this.with(firstMondayOfYearAdjusters)
+
+    if(this.isBefore(firstMondayOfYear)) {
+        val previousMonday = this.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+        return previousMonday.minusWeeks(12)
+    }
+
+    val weekDiff = (this.dayOfYear - firstMondayOfYear.dayOfYear) / 7
+
+    val quarter = (weekDiff - 1) / 13
+    val weeks = quarter * 13L
+
+    return firstMondayOfYear.plusWeeks(weeks)
 }
 
 fun LocalDate.lastDayOfQuarter(): LocalDate {
-    val week = (this.dayOfYear-1) / 7
-    val quarter = week / 13
-    val weeks = (quarter + 1) * 13L
-    val endWeek = this.withMonth(1).withDayOfMonth(1).plusWeeks(weeks -1)
 
-    return endWeek.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+    val firstMondayOfYear = this.with(firstMondayOfYearAdjusters)
+    if(this.isBefore(firstMondayOfYear)) {
+        return firstMondayOfYear.minusDays(1)
+    }
+
+    val weekDiff = (this.dayOfYear - firstMondayOfYear.dayOfYear) / 7
+
+    val quarter = (weekDiff - 1) / 13
+    val weeks = (quarter + 1) * 13L
+
+    return firstMondayOfYear.plusWeeks(weeks).minusDays(1)
 }
 
 fun LocalDate.firstDayOfWeek() : LocalDate {

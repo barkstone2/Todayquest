51ad3c5bb215b9a1b508ead2a658c951db5da24c
barkstone <barkstone2@gmail.com>
2023-08-31 22:52:32 +0900

    feat: 퀘스트 엔티티 변경 시 오픈서치 인덱스에 도큐먼트 삽입
    검색 처리를 위해 오픈서치 추가
    오픈서치 처리용 엔티티 추가

--- src/main/java/dailyquest/TodayquestApplication.java
+++ src/main/java/dailyquest/TodayquestApplication.java
@@ -3,6 +3,7 @@ package dailyquest;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +17,7 @@ import dailyquest.user.dto.UserPrincipal;
 import javax.sql.DataSource;
 import java.util.Optional;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
 public class TodayquestApplication {
 
 	public static void main(String[] args) {

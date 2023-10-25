1e5da942365ba21cc6dd3091cff3a2191ffb2b90
barkstone <barkstone2@gmail.com>
2023-10-25 14:26:46 +0900

    feat: 날짜 검색 기능 추가

--- src/main/java/dailyquest/quest/repository/QuestRepositoryCustom.java
+++ src/main/java/dailyquest/quest/repository/QuestRepositoryCustom.java
@@ -1,6 +1,14 @@
 package dailyquest.quest.repository;
 
+import dailyquest.quest.entity.Quest;
+import dailyquest.quest.entity.QuestState;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
 public interface QuestRepositoryCustom {
 
     Long getNextSeqByUserId(Long userId);
+    Page<Quest> findQuestsByCondition(Long userId, QuestState state, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 }

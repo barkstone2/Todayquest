72895b5a956d7e57a43a86b289abfbbc22062322
barkstone <barkstone2@gmail.com>
2023-10-25 22:31:43 +0900

    feat: 엘라스틱서치 검색 시 검색 조건 추가
    - Criteria -> Native 쿼리로 변경하고 날짜 범위와 상태 검색 조건 동적으로 추가
    - 검색 키워드 타입에 boost 조건 추가하고 멀티 매치용 필드명 리스트로 반환
    - 퀘스트 문서 엔티티에 필드 추가 및 날짜 포맷 매핑 추가
    - 현재 퀘스트 조회 시 상태 정보만 받도록 변경

--- src/main/java/dailyquest/search/document/QuestDocument.java
+++ src/main/java/dailyquest/search/document/QuestDocument.java
@@ -3,7 +3,6 @@ package dailyquest.search.document;
 import dailyquest.quest.dto.DetailResponse;
 import dailyquest.quest.dto.QuestResponse;
 import jakarta.persistence.Id;
-import lombok.Getter;
 import org.springframework.data.elasticsearch.annotations.*;
 
 import java.time.LocalDateTime;
@@ -13,32 +12,30 @@ import java.util.List;
 @Setting(settingPath = "elastic/quests/setting.json")
 @Document(indexName = "quests")
 public class QuestDocument {
-    @Getter
     @Id
     private final Long id;
 
     @Field(type = FieldType.Text)
     private final String title;
-
     @Field(type = FieldType.Text)
     private final String description;
-
     @Field(type = FieldType.Text)
     private final List<String> detailTitles;
-
     @Field(type = FieldType.Long)
     private final Long userId;
+    @Field(type = FieldType.Keyword)
+    private final String state;
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private final LocalDateTime createdDate;
 
-    @Field(type = FieldType.Date)
-    private final LocalDateTime createDate;
-
-    public QuestDocument(Long id, String title, String description, List<String> detailTitles, Long userId, LocalDateTime createDate) {
+    public QuestDocument(Long id, String title, String description, List<String> detailTitles, Long userId, String state, LocalDateTime createdDate) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.detailTitles = detailTitles;
         this.userId = userId;
-        this.createDate = createDate;
+        this.state = state;
+        this.createdDate = createdDate;
     }
 
     public static QuestDocument mapToDocument(QuestResponse questResponse, Long userId) {
@@ -46,7 +43,7 @@ public class QuestDocument {
         if(detailQuests == null) detailQuests = new ArrayList<>();
 
         List<String> detailTitles = detailQuests.stream().map(DetailResponse::getTitle).toList();
-        return new QuestDocument(questResponse.getId(), questResponse.getTitle(), questResponse.getDescription(), detailTitles, userId, questResponse.getCreatedDate());
+        return new QuestDocument(questResponse.getId(), questResponse.getTitle(), questResponse.getDescription(), detailTitles, userId, questResponse.getState().name(), questResponse.getCreatedDate());
     }
 
 }

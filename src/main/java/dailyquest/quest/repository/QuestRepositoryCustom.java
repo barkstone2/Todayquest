e6e41cb4afda78e0fad66f484060826d51d08bc4
barkstone <barkstone2@gmail.com>
2023-10-30 14:04:24 +0900

    feat: 퀘스트 조건 검색 시 개별 파라미터 대신 dto를 인자로 전달

--- src/main/java/dailyquest/quest/repository/QuestRepositoryCustom.java
+++ src/main/java/dailyquest/quest/repository/QuestRepositoryCustom.java
@@ -1,14 +1,12 @@
 package dailyquest.quest.repository;
 
+import dailyquest.quest.dto.QuestSearchCondition;
 import dailyquest.quest.entity.Quest;
-import dailyquest.quest.entity.QuestState;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDateTime;
 
 public interface QuestRepositoryCustom {
 
     Long getNextSeqByUserId(Long userId);
-    Page<Quest> findQuestsByCondition(Long userId, QuestState state, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+    Page<Quest> findQuestsByCondition(Long userId, QuestSearchCondition searchCondition, Pageable pageable);
 }

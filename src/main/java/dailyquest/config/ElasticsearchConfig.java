e7886a3be19aec3041f6eeeff3d53ac7d6dade54
barkstone <barkstone2@gmail.com>
2023-10-31 15:23:22 +0900

    feat: 엘라스틱서치 SSL 설정 추가

--- src/main/java/dailyquest/config/ElasticsearchConfig.java
+++ src/main/java/dailyquest/config/ElasticsearchConfig.java
@@ -2,11 +2,20 @@ package dailyquest.config;
 
 import dailyquest.properties.ElasticProperties;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 @EnableElasticsearchRepositories
 @RequiredArgsConstructor
@@ -18,10 +27,17 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
     @NotNull
     @Override
     public ClientConfiguration clientConfiguration() {
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContextBuilder.create().loadTrustMaterial(new File(elasticProperties.getTruststore().getLocation()), elasticProperties.getTruststore().getPassword().toCharArray()).build();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException | CertificateException |
+                 IOException e) {
+            throw new RuntimeException(e);
+        }
 
         return ClientConfiguration.builder()
                 .connectedTo(elasticProperties.combineHostAndPort())
-                .usingSsl()
+                .usingSsl(sslContext)
                 .withBasicAuth(elasticProperties.getUsername(), elasticProperties.getPassword())
                 .build();
     }

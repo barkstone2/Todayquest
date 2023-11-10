a034142bd5c926a742f9fa3134b72d455ce6b5b8
barkstone <barkstone2@gmail.com>
2023-11-10 14:12:42 +0900

    test: 통합 테스트 별로 컨텍스트 분리

--- src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
+++ src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
@@ -3,8 +3,20 @@ package dailyquest.admin.controller
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import dailyquest.admin.controller.AdminApiControllerTest.AdminApiControllerIntegrationConfig
+import dailyquest.admin.dto.SystemSettingsRequest
+import dailyquest.admin.dto.SystemSettingsResponse
+import dailyquest.admin.service.AdminService
+import dailyquest.common.ResponseData
+import dailyquest.context.IntegrationTestContextBaseConfig
+import dailyquest.context.RedisTestContextConfig
+import dailyquest.jwt.JwtTokenProvider
+import dailyquest.user.dto.RoleType
+import dailyquest.user.entity.ProviderType
+import dailyquest.user.entity.UserInfo
+import dailyquest.user.repository.UserRepository
 import jakarta.servlet.http.Cookie
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -12,38 +24,42 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.filter.CharacterEncodingFilter
-import dailyquest.admin.dto.SystemSettingsRequest
-import dailyquest.admin.dto.SystemSettingsResponse
-import dailyquest.admin.service.AdminService
-import dailyquest.common.ResponseData
-import dailyquest.jwt.JwtTokenProvider
-import dailyquest.user.dto.RoleType
-import dailyquest.user.entity.ProviderType
-import dailyquest.user.entity.UserInfo
-import dailyquest.user.repository.UserRepository
 
 @Suppress("DEPRECATION")
 @DisplayName("관리자 API 컨트롤러 통합 테스트")
 @Transactional
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = [AdminApiControllerIntegrationConfig::class]
 )
 class AdminApiControllerTest @Autowired constructor(
     val adminService: AdminService,
     val userRepository: UserRepository,
     val context: WebApplicationContext,
 ) {
+
+
+    @ComponentScan(basePackages = ["dailyquest.admin"])
+    @Import(
+        IntegrationTestContextBaseConfig::class,
+        RedisTestContextConfig::class,
+    )
+    class AdminApiControllerIntegrationConfig
 
     companion object {
         const val SERVER_ADDR = "http://localhost:"

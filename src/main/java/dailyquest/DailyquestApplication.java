b15b842206c16c3a37e4279d8cff1912fb2054c2
barkstone <barkstone2@gmail.com>
2023-11-10 14:02:36 +0900

    feat: 메인 클래스에 있던 빈 설정 별도 파일로 분리
    - 메인 클래스 이름 변경
    - 불필요한 설정 애노테이션 제거

---
+++ src/main/java/dailyquest/DailyquestApplication.java
@@ -0,0 +1,14 @@
+package dailyquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
+public class DailyquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyquestApplication.class, args);
+	}
+
+}

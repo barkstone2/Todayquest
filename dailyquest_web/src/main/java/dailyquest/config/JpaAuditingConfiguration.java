e2b5f44a0cfa6487b482ba5441881cdbc9f90771
barkstone <barkstone2@gmail.com>
2023-11-21 10:11:07 +0900

    feat: createdBy 미사용으로 인해 해당 Auditing 설정 제거

--- dailyquest_web/src/main/java/dailyquest/config/JpaAuditingConfiguration.java
+++ dailyquest_web/src/main/java/dailyquest/config/JpaAuditingConfiguration.java
@@ -1,25 +1,9 @@
 package dailyquest.config;
 
-import dailyquest.user.dto.UserPrincipal;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfiguration {
-    @Bean
-    public AuditorAware<Long> auditorProvider() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (null == authentication || !authentication.isAuthenticated()) {
-            return null;
-        }
-        UserPrincipal user = (UserPrincipal) authentication.getPrincipal();
-        return () -> Optional.of(user.getId());
-    }
 }

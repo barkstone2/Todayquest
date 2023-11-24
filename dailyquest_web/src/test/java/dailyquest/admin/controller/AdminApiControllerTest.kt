9783bd4dfab5d02ccfd63b3f152280c609b40043
barkstone <barkstone2@gmail.com>
2023-11-24 23:27:06 +0900

    test: 레디스 테스트 시에 테스트 컨테이너를 사용하도록 변경
    - 워크 플로우에서 레디스 초기화 로직 제거

--- dailyquest_web/src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
+++ dailyquest_web/src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
@@ -7,20 +7,18 @@ import dailyquest.admin.controller.AdminApiControllerTest.AdminApiControllerInte
 import dailyquest.admin.dto.SystemSettingsRequest
 import dailyquest.admin.dto.SystemSettingsResponse
 import dailyquest.admin.service.AdminService
+import dailyquest.common.CustomRedisContainer
 import dailyquest.common.ResponseData
 import dailyquest.context.IntegrationTestContextBaseConfig
 import dailyquest.context.RedisTestContextConfig
 import dailyquest.jwt.JwtTokenProvider
-import dailyquest.user.entity.RoleType
 import dailyquest.user.entity.ProviderType
+import dailyquest.user.entity.RoleType
 import dailyquest.user.entity.UserInfo
 import dailyquest.user.repository.UserRepository
 import jakarta.servlet.http.Cookie
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
@@ -39,7 +37,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.filter.CharacterEncodingFilter
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @Suppress("DEPRECATION")
 @DisplayName("관리자 API 컨트롤러 통합 테스트")
 @Transactional
@@ -64,6 +65,16 @@ class AdminApiControllerTest @Autowired constructor(
     companion object {
         const val SERVER_ADDR = "http://localhost:"
         const val URI_PREFIX = "/admin/api/v1"
+
+        @JvmStatic
+        @Container
+        val redis = CustomRedisContainer()
+
+        @BeforeAll
+        @JvmStatic
+        fun initRedis() {
+            redis.initRedis()
+        }
     }
 
     @LocalServerPort

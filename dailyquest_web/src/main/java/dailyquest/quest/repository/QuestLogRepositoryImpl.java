19a3f72595573838bcbafae0ea11a975de4702d6
barkstone <barkstone2@gmail.com>
2023-12-02 21:45:06 +0900

    fix: 등록한 퀘스트 수 처리 오류 수정
    - 불필요한 삭제 상태 조건 제거
    - 등록 퀘스트 수를 PROCEED 로그 수로 판단

--- dailyquest_web/src/main/java/dailyquest/quest/repository/QuestLogRepositoryImpl.java
+++ dailyquest_web/src/main/java/dailyquest/quest/repository/QuestLogRepositoryImpl.java
@@ -87,8 +87,7 @@ public class QuestLogRepositoryImpl implements QuestLogRepositoryCustom {
         List<Tuple> fetch = query
                 .select(questLog.state, questLog.count())
                 .from(questLog)
-                .where(questLog.userId.eq(userId),
-                        questLog.state.eq(QuestState.DELETE).not())
+                .where(questLog.userId.eq(userId))
                 .groupBy(questLog.state)
                 .fetch();
 
@@ -101,11 +100,11 @@ public class QuestLogRepositoryImpl implements QuestLogRepositoryCustom {
             QuestState questState = tuple.get(questLog.state);
             Long count = tuple.get(questLog.count());
             switch (Objects.requireNonNull(questState)) {
-                case COMPLETE -> completedCount += count;
-                case DISCARD -> discardedCount += count;
-                case FAIL -> failedCount += count;
+                case COMPLETE -> completedCount = count;
+                case DISCARD -> discardedCount = count;
+                case FAIL -> failedCount = count;
+                case PROCEED -> registeredCount = count;
             }
-            registeredCount += count;
         }
 
         return new StatusResponse(registeredCount, completedCount, discardedCount, failedCount);

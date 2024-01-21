fd7b4905198d467b8c06885dbdf0ea00f3ba0b68
barkstone <barkstone2@gmail.com>
2024-01-21 21:33:29 +0900

    feat: JWT 토큰 설정값 프로퍼티로 분리
    코드 수정에 따른 테스트 코드 수정

---
+++ dailyquest_web/src/main/java/dailyquest/properties/JwtTokenProperties.kt
@@ -0,0 +1,28 @@
+package dailyquest.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import java.util.StringTokenizer
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+class JwtTokenProperties {
+    var accessTokenValidationMillisecondString = ""
+    var refreshTokenValidationMillisecondString = ""
+    var accessTokenName = ""
+    var refreshTokenName = ""
+    val accessTokenValidationMillisecond
+        get() = parseToMillisecond(accessTokenValidationMillisecondString)
+    val refreshTokenValidationMillisecond
+        get() = parseToMillisecond(refreshTokenValidationMillisecondString)
+
+    private fun parseToMillisecond(millisecondString: String): Long {
+        var millisecond = 0L
+        val tokenizer = StringTokenizer(millisecondString, " * ")
+        while(tokenizer.hasMoreTokens()) {
+            val token = tokenizer.nextToken().toLong()
+            millisecond = if(millisecond == 0L) token else millisecond * token
+        }
+        return millisecond
+    }
+}

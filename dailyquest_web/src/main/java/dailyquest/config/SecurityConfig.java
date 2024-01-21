0e6c1da7ff46e945e4b4cc6ed34f18a333e69940
barkstone <barkstone2@gmail.com>
2024-01-21 17:58:26 +0900

    style: 시큐리티 설정 메서드 체인 스타일 변경

--- dailyquest_web/src/main/java/dailyquest/config/SecurityConfig.java
+++ dailyquest_web/src/main/java/dailyquest/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package dailyquest.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -32,9 +34,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().cors();
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults());
 
         http.authorizeHttpRequests()
                 .requestMatchers(securityUrlProperties.getAllowedUrl()).permitAll()

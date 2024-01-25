b5a93bf4248fbda23e26446c113b123a8cc24694
barkstone <barkstone2@gmail.com>
2024-01-25 16:47:46 +0900

    feat: 비동기 메서드 예외 처리 핸들러 추가
    예외 발생 시 로깅을 위한 로그백 설정 추가

--- dailyquest_web/src/main/java/dailyquest/config/AsyncConfig.java
+++ dailyquest_web/src/main/java/dailyquest/config/AsyncConfig.java
@@ -1,10 +1,17 @@
 package dailyquest.config;
 
+import dailyquest.exception.ElasticAsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new ElasticAsyncUncaughtExceptionHandler();
+    }
 }

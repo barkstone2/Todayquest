94128f62115636b589dc741bea3b2a1f17f9a4f1
barkstone <barkstone2@gmail.com>
2024-02-01 15:17:54 +0900

    feat: 선호 퀘스트 DTO 추가

---
+++ dailyquest_web/src/main/java/dailyquest/preferencequest/dto/PreferenceDetailResponse.kt
@@ -0,0 +1,31 @@
+package dailyquest.preferencequest.dto
+
+import dailyquest.preferencequest.entity.PreferenceDetailQuest
+import dailyquest.quest.dto.DetailRequest
+import dailyquest.quest.entity.DetailQuestType
+
+data class PreferenceDetailResponse(
+    val id: Long = 0,
+    val title: String = "",
+    val targetCount: Int = 1,
+    val type: DetailQuestType = DetailQuestType.CHECK,
+) {
+    companion object {
+        fun createDto(pdq: PreferenceDetailQuest): PreferenceDetailResponse {
+            return PreferenceDetailResponse(
+                id = pdq.id,
+                title = pdq.title,
+                targetCount = pdq.targetCount,
+                type = pdq.type,
+            )
+        }
+    }
+
+    fun mapToDetailRequest(): DetailRequest {
+        return DetailRequest(
+            title = title,
+            type = type,
+            targetCount = targetCount,
+        )
+    }
+}

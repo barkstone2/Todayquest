293610ef0e1ee5e337318d8e1d2c7dcfb02a3c89
barkstone <barkstone2@gmail.com>
2024-02-04 21:50:02 +0900

    refactor: 퀘스트 DTO 리팩토링
    - Jackson Kotlin Module 라이브러리 추가
    - 세부 퀘스트 DTO 리팩토링
    - 퀘스트 DTO 리팩토링
    - DTO 프로퍼티를 주 생성자 안으로 이동 및 기본값 지정
    - 애노테이션에 use-site 타겟 추가
    - 관련 테스트 코드 수정
    - 퀘스트 상태 프로퍼티가 불변이 됨에 따라 서비스 코드 수정

--- dailyquest_web/src/main/java/dailyquest/quest/dto/DetailResponse.kt
+++ dailyquest_web/src/main/java/dailyquest/quest/dto/DetailResponse.kt
@@ -5,17 +5,17 @@ import dailyquest.quest.entity.DetailQuestState
 import dailyquest.quest.entity.DetailQuestType
 
 data class DetailResponse(
-    val id: Long? = null,
-    val title: String? = null,
-    val targetCount: Int? = null,
-    val count: Int? = null,
-    val type: DetailQuestType? = null,
-    val state: DetailQuestState? = null,
-    var canCompleteParent: Boolean? = false,
+    val id: Long = 0,
+    val title: String = "",
+    val targetCount: Int = 1,
+    val count: Int = 0,
+    val type: DetailQuestType = DetailQuestType.CHECK,
+    val state: DetailQuestState = DetailQuestState.PROCEED,
+    val canCompleteParent: Boolean = false,
 ) {
 
     companion object {
-        fun createDto(dq: DetailQuest, canCompleteParent: Boolean? = false): DetailResponse {
+        fun createDto(dq: DetailQuest, canCompleteParent: Boolean = false): DetailResponse {
             return DetailResponse(
                 id = dq.id,
                 title = dq.title,

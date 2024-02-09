d13b15b13b9128cab4ae245ea49d452d0dee66bd
barkstone <barkstone2@gmail.com>
2024-02-09 21:36:07 +0900

    feat: 선호 퀘스트 조회 시 사용된 횟수 반환
    - JPQL JOIN을 통해 카운트 값과 함께 DTO로 반환
    - 응답 DTO에 사용 횟수 프로퍼티 추가 및 부 생성자 추가
    - 서비스 레이어에서 호출되는 메서드 명 변경

--- dailyquest_web/src/main/java/dailyquest/preferencequest/repository/PreferenceQuestRepository.kt
+++ dailyquest_web/src/main/java/dailyquest/preferencequest/repository/PreferenceQuestRepository.kt
@@ -1,11 +1,20 @@
 package dailyquest.preferencequest.repository
 
+import dailyquest.preferencequest.dto.PreferenceQuestResponse
 import dailyquest.preferencequest.entity.PreferenceQuest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PreferenceQuestRepository : JpaRepository<PreferenceQuest, Long> {
-    fun findAllByUserIdAndDeletedDateIsNull(userId: Long): List<PreferenceQuest>
     fun findByIdAndUserIdAndDeletedDateIsNull(preferenceQuestId: Long, userId: Long): PreferenceQuest?
+
+    @Query("select new dailyquest.preferencequest.dto.PreferenceQuestResponse(pq, count(q.id)) " +
+            "from PreferenceQuest pq " +
+            "left join Quest q on q.preferenceQuest.id = pq.id " +
+            "where pq.user.id = :userId and pq.deletedDate is null " +
+            "group by pq.id")
+    fun findAllWithUsedCount(@Param("userId") userId: Long): List<PreferenceQuestResponse>
 }

7e0db05609ec61820c89928e999ff78cbdd76fd4
barkstone <barkstone2@gmail.com>
2024-02-21 16:28:29 +0900

    refactor: PathVariable DTO에 담아서 전달
    - 세부 퀘스트 상호작용 시 PathVariable을 DTO에 담아서 전달

--- dailyquest_web/src/main/java/dailyquest/quest/dto/DetailInteractRequest.kt
+++ dailyquest_web/src/main/java/dailyquest/quest/dto/DetailInteractRequest.kt
@@ -2,7 +2,17 @@ package dailyquest.quest.dto
 
 import org.hibernate.validator.constraints.Range
 
-class DetailInteractRequest(
-    @field:Range(min = 1, max = 255, message = "{Range.details.count}")
-    val count: Int = 1
-)
+data class DetailInteractRequest(
+    @Range(min = 0, max = 255, message = "{Range.details.count}")
+    val count: Int? = null
+){
+    var questId: Long = 0
+        private set
+    var detailQuestId: Long = 0
+        private set
+
+    fun setPathVariables(questId: Long, detailQuestId: Long) {
+        this.questId = questId
+        this.detailQuestId = detailQuestId
+    }
+}

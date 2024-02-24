7a9e5e94666af3b325edb2affe8c9daa6c068e2d
barkstone <barkstone2@gmail.com>
2024-02-24 22:29:26 +0900

    feat: 유저 레벨 달성 업적 처리 로직 추가

--- dailyquest_web/src/test/java/dailyquest/achivement/service/AchievementServiceUnitTest.kt
+++ dailyquest_web/src/test/java/dailyquest/achivement/service/AchievementServiceUnitTest.kt
@@ -6,6 +6,7 @@ import dailyquest.achievement.service.AchievementLogService
 import dailyquest.achievement.service.AchievementQueryService
 import dailyquest.achievement.service.AchievementService
 import dailyquest.quest.service.QuestLogService
+import dailyquest.user.dto.UserPrincipal
 import dailyquest.user.service.UserService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -46,9 +47,9 @@ class AchievementServiceUnitTest {
             doAnswer { savedAchievements.addAll(it.getArgument(0)) }.`when`(achievementLogService).achieveAll(any(), any())
         }
 
-        @DisplayName("QUEST_TOTAL_REGISTRATION 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다")
+        @DisplayName("QUEST_TOTAL_REGISTRATION 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다")
         @Test
-        fun `QUEST_TOTAL_REGISTRATION 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다`() {
+        fun `QUEST_TOTAL_REGISTRATION 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다`() {
             //given
             val achievementType = AchievementType.QUEST_TOTAL_REGISTRATION
             val userId = 1L
@@ -66,9 +67,9 @@ class AchievementServiceUnitTest {
             assertThat(savedAchievements).isNotEmpty.containsAll(notAchievedAchievements)
         }
 
-        @DisplayName("QUEST_TOTAL_COMPLETION 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다")
+        @DisplayName("QUEST_TOTAL_COMPLETION 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다")
         @Test
-        fun `QUEST_TOTAL_COMPLETION 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다`() {
+        fun `QUEST_TOTAL_COMPLETION 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다`() {
             //given
             val achievementType = AchievementType.QUEST_TOTAL_COMPLETION
             val userId = 1L
@@ -86,9 +87,9 @@ class AchievementServiceUnitTest {
             assertThat(savedAchievements).isNotEmpty.containsAll(notAchievedAchievements)
         }
 
-        @DisplayName("QUEST_CONTINUOUS_REGISTRATION_DAYS 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다")
+        @DisplayName("QUEST_CONTINUOUS_REGISTRATION_DAYS 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다")
         @Test
-        fun `QUEST_CONTINUOUS_REGISTRATION_DAYS 타입에 대한 요청인 경우 반환값이 목표값보다 크거나 같은 업적이 달성된다`() {
+        fun `QUEST_CONTINUOUS_REGISTRATION_DAYS 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다`() {
             //given
             val achievementType = AchievementType.QUEST_CONTINUOUS_REGISTRATION_DAYS
             val userId = 1L
@@ -98,6 +99,28 @@ class AchievementServiceUnitTest {
             )
             doReturn(notAchievedAchievements).`when`(achievementQueryService).getNotAchievedAchievements(any(), any())
             doReturn(1).`when`(questLogService).getContinuousRegistrationCount(any(), any())
+
+            //when
+            achievementService.checkAndAchieveAchievements(achievementType, userId)
+
+            //then
+            assertThat(savedAchievements).isNotEmpty.containsAll(notAchievedAchievements)
+        }
+
+        @DisplayName("USER_LEVEL 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다")
+        @Test
+        fun `USER_LEVEL 타입에 대한 요청인 경우 목표값이 반환값보다 작거나 같은 업적이 달성된다`() {
+            //given
+            val achievementType = AchievementType.USER_LEVEL
+            val userId = 1L
+            val notAchievedAchievements = listOf(
+                Achievement(achievementType, 0),
+                Achievement(achievementType, 1),
+            )
+            doReturn(notAchievedAchievements).`when`(achievementQueryService).getNotAchievedAchievements(any(), any())
+            val user = mock<UserPrincipal>()
+            doReturn(user).`when`(userService).getUserById(any())
+            doReturn(1).`when`(user).level
 
             //when
             achievementService.checkAndAchieveAchievements(achievementType, userId)

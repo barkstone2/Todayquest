827a64aa93507bbb30b4915d35cd05a98a70fa77
barkstone <barkstone2@gmail.com>
2024-02-25 16:36:30 +0900

    feat: 골드 획득 업적 처리 기능 추가

--- dailyquest_domain/src/main/java/dailyquest/achievement/entity/AchievementType.kt
+++ dailyquest_domain/src/main/java/dailyquest/achievement/entity/AchievementType.kt
@@ -2,5 +2,5 @@ package dailyquest.achievement.entity
 
 enum class AchievementType {
     QUEST_TOTAL_REGISTRATION, QUEST_TOTAL_COMPLETION, QUEST_CONTINUOUS_REGISTRATION_DAYS,
-    USER_LEVEL
+    USER_LEVEL, USER_GOLD_EARN
 }

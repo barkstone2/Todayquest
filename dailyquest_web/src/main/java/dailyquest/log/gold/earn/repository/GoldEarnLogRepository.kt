7bfccaccf222ade0a5d6d40442b9b4dbb93c5789
barkstone <barkstone2@gmail.com>
2024-02-25 16:10:20 +0900

    feat: 골드 로그 서비스, 리포지토리 추가

---
+++ dailyquest_web/src/main/java/dailyquest/log/gold/earn/repository/GoldEarnLogRepository.kt
@@ -0,0 +1,16 @@
+package dailyquest.log.gold.earn.repository
+
+import dailyquest.log.gold.earn.entity.GoldEarnLog
+import dailyquest.log.gold.earn.entity.GoldEarnSource
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface GoldEarnLogRepository: JpaRepository<GoldEarnLog, Long> {
+
+    @Query("select coalesce(sum(gel.amount), 0) from GoldEarnLog gel where gel.user.id = :userId")
+    fun getTotalGoldEarnOfUser(@Param("userId") userId: Long): Int
+
+    @Query("select coalesce(sum(gel.amount), 0) from GoldEarnLog gel where gel.user.id = :userId and gel.source = :source")
+    fun getTotalGoldEarnOfUserFromSource(@Param("userId") userId: Long, @Param("source") source: GoldEarnSource): Int
+}

f1827008fdfe1a3a34357be4f0d9b25735935c63
barkstone <barkstone2@gmail.com>
2024-02-25 16:36:30 +0900

    feat: 골드 획득 업적 처리 기능 추가

--- dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementService.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementService.kt
@@ -2,6 +2,7 @@ package dailyquest.achievement.service
 
 import dailyquest.achievement.entity.Achievement
 import dailyquest.achievement.entity.AchievementType
+import dailyquest.log.gold.earn.service.GoldEarnLogService
 import dailyquest.quest.service.QuestLogService
 import dailyquest.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,14 +15,16 @@ class AchievementService @Autowired constructor(
     val achievementQueryService: AchievementQueryService,
     val questLogService: QuestLogService,
     val achievementLogService: AchievementLogService,
-    val userService: UserService
+    val userService: UserService,
+    val userGoldEarnLogService: GoldEarnLogService
 ) {
 
     fun checkAndAchieveAchievements(achievementType: AchievementType, userId: Long) {
         val notAchievedAchievements = achievementQueryService.getNotAchievedAchievements(achievementType, userId)
         val achievableAchievements = mutableListOf<Achievement>()
         for (targetAchievement in notAchievedAchievements) {
-            if (this.canAchieve(targetAchievement, userId)) {
+            val currentValue = this.resolveCurrentValue(targetAchievement, userId)
+            if (targetAchievement.canAchieve(currentValue)) {
                 achievableAchievements.add(targetAchievement)
             } else {
                 break
@@ -31,14 +34,13 @@ class AchievementService @Autowired constructor(
         achievementLogService.achieveAll(achievableAchievements, userId)
     }
 
-    private fun canAchieve(targetAchievement: Achievement, userId: Long): Boolean {
-        val targetValue = targetAchievement.targetValue
-        val currentValue = when (targetAchievement.type) {
+    private fun resolveCurrentValue(targetAchievement: Achievement, userId: Long): Int {
+        return when (targetAchievement.type) {
             AchievementType.QUEST_TOTAL_REGISTRATION -> questLogService.getTotalRegistrationCount(userId)
             AchievementType.QUEST_TOTAL_COMPLETION -> questLogService.getTotalCompletionCount(userId)
-            AchievementType.QUEST_CONTINUOUS_REGISTRATION_DAYS -> questLogService.getContinuousRegistrationCount(userId, targetValue)
+            AchievementType.QUEST_CONTINUOUS_REGISTRATION_DAYS -> questLogService.getContinuousRegistrationCount(userId, targetAchievement.targetValue)
             AchievementType.USER_LEVEL -> userService.getUserById(userId).level
+            AchievementType.USER_GOLD_EARN -> userGoldEarnLogService.getTotalGoldEarnOfUser(userId)
         }
-        return currentValue >= targetValue
     }
 }

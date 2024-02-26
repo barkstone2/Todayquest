a5ac29051ac294f5d17e2d4b43c4870c8b281cf4
barkstone <barkstone2@gmail.com>
2024-02-27 00:17:02 +0900

    refactor: 레디스 호출 로직 추상화

---
+++ dailyquest_web/src/main/java/dailyquest/redis/repository/RedisRepository.kt
@@ -0,0 +1,46 @@
+package dailyquest.redis.repository
+
+import dailyquest.common.MessageUtil
+import dailyquest.exception.RedisDataNotFoundException
+import dailyquest.properties.RedisKeyProperties
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class RedisRepository(
+    val redisTemplate: RedisTemplate<String, String>,
+    val redisKeyProperties: RedisKeyProperties
+) {
+
+    fun getExpTable(): Map<Int, Long> {
+        return redisTemplate.opsForHash<Int, Long>().entries(redisKeyProperties.expTable)
+    }
+
+    fun getQuestClearExp(): Long {
+        return this.getSettingValue(redisKeyProperties.questClearExp)
+    }
+
+    fun getQuestClearGold(): Long {
+        return this.getSettingValue(redisKeyProperties.questClearExp)
+    }
+
+    private fun getSettingValue(key: String): Long {
+        val boundHashOps = redisTemplate.boundHashOps<String, Long>(redisKeyProperties.settings)
+        return boundHashOps.get(key) ?: throw RedisDataNotFoundException(MessageUtil.getMessage("exception.server.error"))
+    }
+
+    fun getRandomNicknamePrefix(): String {
+        return this.getRandomSetMember(redisKeyProperties.nicknamePrefix)
+    }
+
+    fun getRandomNicknamePostfix(): String {
+        return this.getRandomSetMember(redisKeyProperties.nicknamePostfix)
+    }
+
+    private fun getRandomSetMember(setKey: String): String {
+        return redisTemplate.opsForSet().randomMember(setKey)
+    }
+
+
+
+}
--- dailyquest_web/src/main/java/dailyquest/user/service/UserService.java
+++ dailyquest_web/src/main/java/dailyquest/user/service/UserService.java
@@ -1,23 +1,16 @@
 package dailyquest.user.service;
 
-import dailyquest.user.dto.UserExpAndGoldRequest;
-import dailyquest.user.dto.UserSaveRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.BoundHashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import dailyquest.common.MessageUtil;
-import dailyquest.exception.RedisDataNotFoundException;
-import dailyquest.properties.RedisKeyProperties;
 import dailyquest.quest.entity.QuestType;
+import dailyquest.redis.service.RedisService;
+import dailyquest.user.dto.UserExpAndGoldRequest;
 import dailyquest.user.dto.UserPrincipal;
+import dailyquest.user.dto.UserSaveRequest;
 import dailyquest.user.dto.UserUpdateRequest;
 import dailyquest.user.entity.ProviderType;
 import dailyquest.user.entity.UserInfo;
-
-import java.util.Map;
-import java.util.Random;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @RequiredArgsConstructor
@@ -25,34 +18,30 @@ import java.util.Random;
 public class UserService {
     private final UserQueryService userQueryService;
     private final UserCommandService userCommandService;
-    private final RedisTemplate<String, String> redisTemplate;
-    private final RedisKeyProperties redisKeyProperties;
+    private final RedisService redisService;
 
     public UserPrincipal getOrRegisterUser(String oauth2Id, ProviderType providerType) {
         UserInfo foundUser = userQueryService.findUser(oauth2Id);
         boolean needSave = foundUser == null;
         if (needSave) {
             String randomNickname = this.createRandomNickname();
-            while (userQueryService.isDuplicateNickname(randomNickname)) {
-                randomNickname = this.createRandomNickname();
-            }
             UserSaveRequest saveRequest = new UserSaveRequest(oauth2Id, randomNickname, providerType);
             foundUser = userCommandService.saveUser(saveRequest);
         }
-        Map<Integer, Long> expTable = redisTemplate.<Integer, Long>opsForHash().entries(redisKeyProperties.getExpTable());
-        return UserPrincipal.create(foundUser, expTable);
+        return UserPrincipal.create(foundUser, redisService.getExpTable());
     }
 
     private String createRandomNickname() {
-        String nicknamePrefix = redisTemplate.opsForSet().randomMember(redisKeyProperties.getNicknamePrefix());
-        String nicknamePostfix = redisTemplate.opsForSet().randomMember(redisKeyProperties.getNicknamePostfix());
-        return nicknamePrefix + nicknamePostfix + new Random().nextInt(1_000_000_000);
+        String candidateNickname = redisService.createRandomNickname();
+        while (userQueryService.isDuplicateNickname(candidateNickname)) {
+            candidateNickname = redisService.createRandomNickname();
+        }
+        return candidateNickname;
     }
 
     public UserPrincipal getUserById(Long userId) {
         UserInfo userInfo = userQueryService.findUser(userId);
-        Map<Integer, Long> expTable = redisTemplate.<Integer, Long>opsForHash().entries(redisKeyProperties.getExpTable());
-        return UserPrincipal.create(userInfo, expTable);
+        return UserPrincipal.create(userInfo, redisService.getExpTable());
     }
 
     public void changeUserSettings(UserPrincipal principal, UserUpdateRequest updateRequest) {
@@ -61,13 +50,10 @@ public class UserService {
     }
 
     public void earnExpAndGold(QuestType type, UserInfo user) {
-        BoundHashOperations<String, String, Long> ops = redisTemplate.boundHashOps(redisKeyProperties.getSettings());
-        Long questClearExp = ops.get(redisKeyProperties.getQuestClearExp());
-        Long questClearGold = ops.get(redisKeyProperties.getQuestClearGold());
-        if (questClearExp == null || questClearGold == null) {
-            throw new RedisDataNotFoundException(MessageUtil.getMessage("exception.server.error"));
-        }
+        UserInfo foundUser = userQueryService.findUser(user.getId());
+        long questClearExp = redisService.getQuestClearExp();
+        long questClearGold = redisService.getQuestClearGold();
         UserExpAndGoldRequest updateRequest = UserExpAndGoldRequest.of(type, questClearExp, questClearGold);
-        userCommandService.updateUserExpAndGold(user, updateRequest);
+        userCommandService.updateUserExpAndGold(foundUser, updateRequest);
     }
 }

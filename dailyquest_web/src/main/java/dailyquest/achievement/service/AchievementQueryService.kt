6e3b4a41d8eab09963340d7ba55e6215a69626f8
barkstone <barkstone2@gmail.com>
2024-02-28 17:36:28 +0900

    rename: 업적 서비스 이름 변경
    - 트랜잭션 애노테이션 부여

--- dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementQueryService.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementQueryService.kt
@@ -4,7 +4,9 @@ import dailyquest.achievement.entity.Achievement
 import dailyquest.achievement.entity.AchievementType
 import dailyquest.achievement.repository.AchievementRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional(readOnly = true)
 @Service
 class AchievementQueryService(
     val achievementRepository: AchievementRepository

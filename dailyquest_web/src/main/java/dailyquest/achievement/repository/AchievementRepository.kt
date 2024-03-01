e2c7e8e5b9c3010efcb423a40c2878c7ffeec4fe
barkstone <barkstone2@gmail.com>
2024-03-01 19:21:03 +0900

    feat: 전체 업적 현황 조회 기능 추가
    - 테스트 코드 추가
    - 스프링 모킹 라이브러리 springmockk 추가

--- dailyquest_web/src/main/java/dailyquest/achievement/repository/AchievementRepository.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/repository/AchievementRepository.kt
@@ -1,5 +1,6 @@
 package dailyquest.achievement.repository
 
+import dailyquest.achievement.dto.AchievementResponse
 import dailyquest.achievement.entity.Achievement
 import dailyquest.achievement.entity.AchievementType
 import org.springframework.data.jpa.repository.JpaRepository
@@ -15,4 +16,12 @@ interface AchievementRepository: JpaRepository<Achievement, Long>{
             "order by a.targetValue " +
             "limit 1")
     fun findNotAchievedAchievement(@Param("type") type: AchievementType, @Param("userId") userId: Long): Achievement?
+
+
+    @Query("select new dailyquest.achievement.dto.AchievementResponse(a.title, a.description, a.type, a.targetValue, (al.id is not null), al.createdDate) " +
+            "from Achievement a " +
+            "left join AchievementLog al " +
+            "on al.user.id = :userId and al.achievement.id = a.id " +
+            "where a.type = :type")
+    fun getAchievementsWithAchieveInfo(@Param("type") type: AchievementType, @Param("userId") userId: Long): List<AchievementResponse>
 }

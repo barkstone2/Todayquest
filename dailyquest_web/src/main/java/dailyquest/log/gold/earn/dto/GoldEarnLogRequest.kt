d956e07b64f1d7e9790c6068710b4b31e16a105c
barkstone <barkstone2@gmail.com>
2024-03-01 21:26:35 +0900

    feat: 골드 획득 업적 처리 기능 추가
    - 테스트 코드 추가
    - 골드 획득이 발생하는 곳에서 로그 삽입 로직 추가

--- dailyquest_web/src/main/java/dailyquest/log/gold/earn/dto/GoldEarnLogRequest.kt
+++ dailyquest_web/src/main/java/dailyquest/log/gold/earn/dto/GoldEarnLogRequest.kt
@@ -3,7 +3,7 @@ package dailyquest.log.gold.earn.dto
 import dailyquest.log.gold.earn.entity.GoldEarnSource
 
 class GoldEarnLogRequest(
-    val amount: Int,
+    val amount: Long,
     val source: GoldEarnSource
 ) {
 }

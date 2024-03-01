b425b8643d936c1601afb032bcf22a431b10268f
barkstone <barkstone2@gmail.com>
2024-03-01 21:26:35 +0900

    feat: 골드 획득 업적 처리 기능 추가
    - 테스트 코드 추가
    - 골드 획득이 발생하는 곳에서 로그 삽입 로직 추가

--- dailyquest_web/src/main/java/dailyquest/user/service/UserCommandService.kt
+++ dailyquest_web/src/main/java/dailyquest/user/service/UserCommandService.kt
@@ -1,15 +1,21 @@
 package dailyquest.user.service
 
+import dailyquest.log.gold.earn.dto.GoldEarnLogRequest
+import dailyquest.log.gold.earn.entity.GoldEarnSource
+import dailyquest.log.gold.earn.service.GoldEarnLogService
 import dailyquest.user.dto.UserExpAndGoldRequest
 import dailyquest.user.dto.UserSaveRequest
 import dailyquest.user.dto.UserUpdateRequest
 import dailyquest.user.entity.UserInfo
 import dailyquest.user.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @Service
 class UserCommandService(
     private val userRepository: UserRepository,
+    private val goldEarnLogService: GoldEarnLogService,
 ) {
     fun saveUser(saveRequest: UserSaveRequest): UserInfo {
         val requestEntity = saveRequest.mapToEntity()
@@ -23,6 +29,10 @@ class UserCommandService(
     }
 
     fun updateUserExpAndGold(updateTarget: UserInfo, updateRequest: UserExpAndGoldRequest) {
-        updateTarget.addExpAndGold(updateRequest.calculateEarnedExp(), updateRequest.calculateEarnedGold())
+        val earnedExp = updateRequest.calculateEarnedExp()
+        val earnedGold = updateRequest.calculateEarnedGold()
+        updateTarget.addExpAndGold(earnedExp, earnedGold)
+        val goldEarnLogRequest = GoldEarnLogRequest(earnedGold, GoldEarnSource.QUEST_COMPLETION)
+        goldEarnLogService.saveGoldEarnLog(updateTarget.id, goldEarnLogRequest)
     }
 }
--- dailyquest_web/src/test/java/dailyquest/context/IntegrationTestContextBaseConfig.java
+++ dailyquest_web/src/test/java/dailyquest/context/IntegrationTestContextBaseConfig.java
@@ -11,28 +11,17 @@ import dailyquest.jwt.JwtTokenProvider;
 import dailyquest.properties.JwtTokenProperties;
 import dailyquest.properties.SecurityOriginProperties;
 import dailyquest.properties.SecurityUrlProperties;
-import dailyquest.redis.repository.RedisRepository;
-import dailyquest.redis.service.RedisService;
-import dailyquest.user.entity.UserInfo;
-import dailyquest.user.repository.UserRepository;
-import dailyquest.user.service.UserCommandService;
-import dailyquest.user.service.UserQueryService;
-import dailyquest.user.service.UserService;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Import({
         DataSourceConfig.class, SecurityConfig.class, WebMvcConfig.class,
         RestApiExceptionHandler.class, JpaAuditingConfiguration.class, MessageUtil.class,
         JwtAuthorizationFilter.class, JwtTokenProvider.class, JwtTokenProperties.class,
         SecurityOriginProperties.class, SecurityUrlProperties.class,
-        UserService.class, UserQueryService.class, UserCommandService.class, RedisService.class, RedisRepository.class
+        UserTestContextConfig.class
 })
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
-@EntityScan(basePackageClasses = UserInfo.class)
 @EnableAutoConfiguration
 @TestConfiguration
 public class IntegrationTestContextBaseConfig {

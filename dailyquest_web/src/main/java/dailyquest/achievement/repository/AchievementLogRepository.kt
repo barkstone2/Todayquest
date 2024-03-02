f25a0465024d217a83f32888478359f3a39bad10
barkstone <barkstone2@gmail.com>
2024-03-02 16:49:48 +0900

    rename: 업적 달성 로그 엔티티 이름을 더 명확하게 변경
    - 서비스와 리포지토리의 이름도 변경

--- dailyquest_web/src/main/java/dailyquest/achievement/repository/AchievementLogRepository.kt
+++
@@ -1,7 +0,0 @@
-package dailyquest.achievement.repository
-
-import dailyquest.achievement.entity.AchievementLog
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AchievementLogRepository: JpaRepository<AchievementLog, Long> {
-}
--- dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementAchieveLogCommandService.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/service/AchievementAchieveLogCommandService.kt
@@ -1,22 +1,22 @@
 package dailyquest.achievement.service
 
 import dailyquest.achievement.entity.Achievement
-import dailyquest.achievement.entity.AchievementLog
-import dailyquest.achievement.repository.AchievementLogRepository
+import dailyquest.achievement.entity.AchievementAchieveLog
+import dailyquest.achievement.repository.AchievementAchieveLogRepository
 import dailyquest.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
 @Service
-class AchievementLogCommandService(
-    private val achievementLogRepository: AchievementLogRepository,
+class AchievementAchieveLogCommandService(
+    private val achieveLogRepository: AchievementAchieveLogRepository,
     private val userRepository: UserRepository,
 ) {
 
     fun achieve(achievableAchievement: Achievement, userId: Long) {
         val user = userRepository.findById(userId).get()
-        val achievementLog = AchievementLog.of(achievableAchievement, user)
-        achievementLogRepository.save(achievementLog)
+        val achieveLog = AchievementAchieveLog.of(achievableAchievement, user)
+        achieveLogRepository.save(achieveLog)
     }
 }

43c48361a5e634544284de5b4b3fc8a16d588327
barkstone <barkstone2@gmail.com>
2024-03-02 16:19:40 +0900

    feat: 신규 업적 등록 기능 추가
    - 요청 DTO 수정
    - 테스트 코드 추가 및 수정

--- dailyquest_web/src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
+++ dailyquest_web/src/test/java/dailyquest/admin/controller/AdminApiControllerTest.kt
@@ -1,155 +1,72 @@
 package dailyquest.admin.controller
 
 import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import dailyquest.admin.controller.AdminApiControllerTest.AdminApiControllerIntegrationConfig
+import dailyquest.achievement.dto.AchievementRequest
+import dailyquest.achievement.entity.AchievementType
+import dailyquest.achievement.repository.AchievementRepository
 import dailyquest.admin.dto.SystemSettingsRequest
 import dailyquest.admin.dto.SystemSettingsResponse
 import dailyquest.admin.service.AdminService
-import dailyquest.common.CustomRedisContainer
 import dailyquest.common.ResponseData
-import dailyquest.context.IntegrationTestContextBaseConfig
-import dailyquest.context.RedisTestContextConfig
+import dailyquest.context.IntegrationTestContextWithRedis
 import dailyquest.jwt.JwtTokenProvider
-import dailyquest.user.entity.ProviderType
-import dailyquest.user.entity.RoleType
-import dailyquest.user.entity.UserInfo
 import dailyquest.user.repository.UserRepository
-import jakarta.servlet.http.Cookie
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
-import org.springframework.web.filter.CharacterEncodingFilter
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 
-@Testcontainers
-@Suppress("DEPRECATION")
 @DisplayName("관리자 API 컨트롤러 통합 테스트")
 @Transactional
-@SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    classes = [AdminApiControllerIntegrationConfig::class]
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AdminApiControllerTest @Autowired constructor(
-    val adminService: AdminService,
-    val userRepository: UserRepository,
-    val context: WebApplicationContext,
-) {
+    context: WebApplicationContext,
+    userRepository: UserRepository,
+    jwtTokenProvider: JwtTokenProvider,
+    private val adminService: AdminService,
+    private val achievementRepository: AchievementRepository,
+): IntegrationTestContextWithRedis(context, userRepository, jwtTokenProvider) {
 
-
-    @ComponentScan(basePackages = ["dailyquest.admin"])
-    @Import(
-        IntegrationTestContextBaseConfig::class,
-        RedisTestContextConfig::class,
-    )
-    class AdminApiControllerIntegrationConfig
-
-    companion object {
-        const val SERVER_ADDR = "http://localhost:"
-        const val URI_PREFIX = "/admin/api/v1"
-
-        @JvmStatic
-        @Container
-        val redis = CustomRedisContainer()
-
-        @BeforeAll
-        @JvmStatic
-        fun initRedis() {
-            redis.initRedis()
-        }
-    }
-
-    @LocalServerPort
-    var port = 0
-
-    @Autowired
-    lateinit var jwtTokenProvider: JwtTokenProvider
-
-    lateinit var mvc: MockMvc
-    lateinit var userToken: Cookie
-    lateinit var adminToken: Cookie
-    val om: ObjectMapper = ObjectMapper().registerModule(JavaTimeModule())
-
-    @BeforeEach
-    fun setUp() {
-        mvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .addFilter<DefaultMockMvcBuilder>(CharacterEncodingFilter("UTF-8", true))
-            .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
-            .build()
-
-        val user = UserInfo("", "user1", ProviderType.GOOGLE)
-        val admin = UserInfo("", "user2", ProviderType.GOOGLE)
-        admin.role = RoleType.ADMIN
-
-        val savedUser = userRepository.save(user)
-        val savedAdmin = userRepository.save(admin)
-
-        val accessToken1 = jwtTokenProvider.createAccessToken(savedUser.id)
-        userToken = jwtTokenProvider.createAccessTokenCookie(accessToken1)
-
-        val accessToken2 = jwtTokenProvider.createAccessToken(savedAdmin.id)
-        adminToken = jwtTokenProvider.createAccessTokenCookie(accessToken2)
-    }
-
+    private val uriPrefix = "/admin/api/v1"
 
     @DisplayName("시스템 설정 조회 시")
     @Nested
     inner class GetSystemSettingsTest {
 
-        private val uriSuffix = "/reward"
+        private val url = "$SERVER_ADDR$port$uriPrefix/reward"
 
         @DisplayName("관리자 권한이 아니면 FORBIDDEN이 반환된다")
         @Test
         fun `관리자 권한이 아니면 FORBIDDEN이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             //when
             val request = mvc
                 .perform(
                     get(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(userToken)
+                        .useUserConfiguration()
                 )
 
             //then
-            request
-                .andExpect(status().isForbidden)
+            request.andExpect(status().isForbidden)
         }
 
         @DisplayName("관리자 권한이라면 시스템 세팅이 반환된다")
         @Test
         fun `관리자 권한이라면 시스템 세팅이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             //when
             val request = mvc
                 .perform(
                     get(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(adminToken)
+                        .useAdminConfiguration()
                 )
 
             //then
@@ -160,21 +77,19 @@ class AdminApiControllerTest @Autowired constructor(
                 .response
                 .contentAsString
 
-            val result = om.readValue(body, object: TypeReference<ResponseData<SystemSettingsResponse>>(){})
+            om.readValue(body, object: TypeReference<ResponseData<SystemSettingsResponse>>(){})
         }
     }
 
     @DisplayName("시스템 설정 변경 시")
     @Nested
     inner class UpdateSystemSettingsTest {
-        private val uriSuffix = "/reward"
+        private val url = "$SERVER_ADDR$port$uriPrefix/reward"
 
         @DisplayName("관리자 권한이 아니면 FORBIDDEN이 반환된다")
         @Test
         fun `관리자 권한이 아니면 FORBIDDEN이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             val requestDto = SystemSettingsRequest(5, 5, 10)
             val requestBody = om.writeValueAsString(requestDto)
 
@@ -182,23 +97,18 @@ class AdminApiControllerTest @Autowired constructor(
             val request = mvc
                 .perform(
                     put(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(userToken)
+                        .useUserConfiguration()
                         .content(requestBody)
                 )
 
             //then
-            request
-                .andExpect(status().isForbidden)
+            request.andExpect(status().isForbidden)
         }
 
         @DisplayName("관리자 권한이라면 시스템 세팅이 변경된다")
         @Test
         fun `관리자 권한이라면 시스템 세팅이 변경된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             val requestDto = SystemSettingsRequest(5, 5, 10)
             val requestBody = om.writeValueAsString(requestDto)
 
@@ -206,63 +116,50 @@ class AdminApiControllerTest @Autowired constructor(
             val request = mvc
                 .perform(
                     put(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(adminToken)
+                        .useAdminConfiguration()
                         .content(requestBody)
                 )
 
             //then
-            request
-                .andExpect(status().isOk)
+            request.andExpect(status().isOk)
 
             val systemSettings = adminService.getSystemSettings()
 
             assertThat(systemSettings.maxRewardCount).isEqualTo(requestDto.maxRewardCount)
             assertThat(systemSettings.questClearExp).isEqualTo(requestDto.questClearExp)
             assertThat(systemSettings.questClearGold).isEqualTo(requestDto.questClearGold)
-
         }
     }
 
     @DisplayName("경험치 테이블 조회 시")
     @Nested
     inner class GetExpTableTest {
+        private val url = "$SERVER_ADDR$port$uriPrefix/exp-table"
 
-        private val uriSuffix = "/exp-table"
         @DisplayName("관리자 권한이 아니면 FORBIDDEN이 반환된다")
         @Test
         fun `관리자 권한이 아니면 FORBIDDEN이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             //when
             val request = mvc
                 .perform(
                     get(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(userToken)
+                        .useUserConfiguration()
                 )
 
             //then
-            request
-                .andExpect(status().isForbidden)
+            request.andExpect(status().isForbidden)
         }
 
         @DisplayName("관리자 권한이라면 경험치 테이블이 반환된다")
         @Test
         fun `관리자 권한이라면 경험치 테이블이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
-
             //when
             val request = mvc
                 .perform(
                     get(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(adminToken)
+                        .useAdminConfiguration()
                 )
 
             //then
@@ -273,7 +170,7 @@ class AdminApiControllerTest @Autowired constructor(
                 .response
                 .contentAsString
 
-            val result = om.readValue(body, object: TypeReference<ResponseData<Map<String, Long>>>(){})
+            om.readValue(body, object: TypeReference<ResponseData<Map<String, Long>>>(){})
         }
     }
 
@@ -281,13 +178,12 @@ class AdminApiControllerTest @Autowired constructor(
     @DisplayName("경험치 테이블 변경 시")
     @Nested
     inner class UpdateExpTableTest {
+        private val url = "$SERVER_ADDR$port$uriPrefix/exp-table"
 
-        private val uriSuffix = "/exp-table"
         @DisplayName("관리자 권한이 아니면 FORBIDDEN이 반환된다")
         @Test
         fun `관리자 권한이 아니면 FORBIDDEN이 반환된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
             val requestMap = mapOf(1 to 5, 2 to 5, 3 to 10)
             val requestBody = om.writeValueAsString(requestMap)
 
@@ -295,9 +191,7 @@ class AdminApiControllerTest @Autowired constructor(
             val request = mvc
                 .perform(
                     put(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(userToken)
+                        .useUserConfiguration()
                         .content(requestBody)
                 )
 
@@ -310,7 +204,6 @@ class AdminApiControllerTest @Autowired constructor(
         @Test
         fun `관리자 권한이라면 경험치 테이블이 변경된다`() {
             //given
-            val url = "$SERVER_ADDR$port$URI_PREFIX$uriSuffix"
             val requestMap = mapOf(1 to 5L, 2 to 5L, 3 to 10L)
             val requestBody = om.writeValueAsString(requestMap)
 
@@ -318,15 +211,12 @@ class AdminApiControllerTest @Autowired constructor(
             val request = mvc
                 .perform(
                     put(url)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .with(csrf())
-                        .cookie(adminToken)
+                        .useAdminConfiguration()
                         .content(requestBody)
                 )
 
             //then
-            request
-                .andExpect(status().isOk)
+            request.andExpect(status().isOk)
 
             val expTable = adminService.getExpTable()
 
@@ -336,5 +226,69 @@ class AdminApiControllerTest @Autowired constructor(
         }
     }
 
+    @DisplayName("신규 업적 등록 시")
+    @Nested
+    inner class TestSaveAchievement {
+        private val url = "$SERVER_ADDR$port$uriPrefix/achievements"
+        private val saveRequest = AchievementRequest("저장", "저장", AchievementType.USER_LEVEL, 1)
 
+        @DisplayName("타입과 목표값이 모두 중복될 경우 400이 반환된다")
+        @Test
+        fun `타입과 목표값이 모두 중복될 경우 400이 반환된다`() {
+            //given
+            achievementRepository.save(saveRequest.mapToEntity())
+            val requestBody = om.writeValueAsString(saveRequest)
+
+            //when
+            val result = mvc.perform {
+                post(url)
+                    .useAdminConfiguration()
+                    .content(requestBody)
+                    .buildRequest(it)
+            }
+
+            //then
+            result.andExpect { status().isBadRequest }
+        }
+
+        @DisplayName("타입과 목표값이 모두 중복될 경우 신규 업적이 등록되지 않는다")
+        @Test
+        fun `타입과 목표값이 모두 중복될 경우 신규 업적이 등록되지 않는다`() {
+            //given
+            achievementRepository.save(saveRequest.mapToEntity())
+            val requestBody = om.writeValueAsString(saveRequest)
+
+            //when
+            mvc.perform {
+                post(url)
+                    .useAdminConfiguration()
+                    .content(requestBody)
+                    .buildRequest(it)
+            }
+
+            //then
+            val filteredResult = achievementRepository.findAll()
+                .filter { it.type == saveRequest.type && it.targetValue == saveRequest.targetValue }
+            assertThat(filteredResult.size).isOne()
+        }
+
+        @DisplayName("타입과 목표값이 중복되지 않으면 신규 업적이 등록된다")
+        @Test
+        fun `타입과 목표값이 중복되지 않으면 신규 업적이 등록된다`() {
+            //given
+            val requestBody = om.writeValueAsString(saveRequest)
+
+            //when
+            mvc.perform {
+                post(url)
+                    .useAdminConfiguration()
+                    .content(requestBody)
+                    .buildRequest(it)
+            }.andExpect { status().isOk }
+
+            //then
+            val allAchievements = achievementRepository.findAll()
+            assertThat(allAchievements).anyMatch { it.type == saveRequest.type && it.targetValue == saveRequest.targetValue }
+        }
+    }
 }

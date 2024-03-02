094db177772a6e37f66d374bf023922b049a4acb
barkstone <barkstone2@gmail.com>
2024-03-02 16:19:40 +0900

    feat: 신규 업적 등록 기능 추가
    - 요청 DTO 수정
    - 테스트 코드 추가 및 수정

--- dailyquest_web/src/main/java/dailyquest/achievement/dto/AchievementRequest.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/dto/AchievementRequest.kt
@@ -1,9 +1,20 @@
 package dailyquest.achievement.dto
 
+import dailyquest.achievement.entity.Achievement
 import dailyquest.achievement.entity.AchievementType
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotEmpty
 
-class AchievementRequest(
+data class AchievementRequest(
+    @field:NotEmpty
+    val title: String,
+    @field:NotEmpty
+    val description: String,
     val type: AchievementType,
-    val currentValue: Int,
+    @field:Min(1)
+    val targetValue: Int,
 ) {
+    fun mapToEntity(): Achievement {
+        return Achievement(title, description, type, targetValue)
+    }
 }

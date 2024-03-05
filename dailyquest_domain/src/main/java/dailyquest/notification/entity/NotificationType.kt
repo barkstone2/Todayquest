aaa1f0c0db3ebcaa899ee002f2e20af9f92d63bb
barkstone <barkstone2@gmail.com>
2024-03-05 15:33:55 +0900

    feat: 알림 엔티티 추가

---
+++ dailyquest_domain/src/main/java/dailyquest/notification/entity/NotificationType.kt
@@ -0,0 +1,5 @@
+package dailyquest.notification.entity
+
+enum class NotificationType(val title: String) {
+    ACHIEVEMENT_ACHIEVE("새로운 업적을 달성했습니다.")
+}
---
+++ dailyquest_web/src/main/java/dailyquest/notification/repository/NotificationRepository.kt
@@ -0,0 +1,7 @@
+package dailyquest.notification.repository
+
+import dailyquest.notification.entity.Notification
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface NotificationRepository: JpaRepository<Notification, Long> {
+}

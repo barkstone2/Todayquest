4fe0f633650a78092765840e4264a3e954b3ecf0
barkstone <barkstone2@gmail.com>
2024-03-07 16:45:33 +0900

    feat: SSE send, emitter 반환 기능 추가

---
+++ dailyquest_web/src/main/java/dailyquest/sse/SseService.kt
@@ -0,0 +1,42 @@
+package dailyquest.sse
+
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.*
+import java.util.concurrent.ConcurrentHashMap
+
+@Async
+@Service
+class SseService {
+    private val sseEmitters = ConcurrentHashMap<Long, SseEmitter>()
+
+    fun findOrCreateEmitter(userId: Long): SseEmitter {
+        val sseEmitter = createEmitter(userId)
+        sseEmitters[userId] = sseEmitter
+        return sseEmitter
+    }
+
+    fun sendNotificationEvent(userId: Long) {
+        val sseEmitter = this.getEmitter(userId)
+        sseEmitter?.send(event().name("notification").data(""))
+    }
+
+    private fun getEmitter(userId: Long): SseEmitter? {
+        return sseEmitters[userId]
+    }
+
+    private fun createEmitter(userId: Long): SseEmitter {
+        val sseEmitter = SseEmitter()
+        sseEmitter.onCompletion {
+            sseEmitters.remove(userId)
+        }
+        sseEmitter.onError {
+            sseEmitter.complete()
+        }
+        sseEmitter.onTimeout {
+            sseEmitter.complete()
+        }
+        return sseEmitter
+    }
+}

acd3061b7e3ff7adacf12a22b1dc8dacca42f28d
barkstone <barkstone2@gmail.com>
2024-03-07 16:45:33 +0900

    feat: SSE send, emitter 반환 기능 추가

---
+++ dailyquest_web/src/main/java/dailyquest/sse/SseController.kt
@@ -0,0 +1,24 @@
+package dailyquest.sse
+
+import dailyquest.user.dto.UserPrincipal
+import org.springframework.http.MediaType
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+@RestController("/api/v1/sse")
+class SseController(
+    private val sseService: SseService
+) {
+
+    @GetMapping(value = [""], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getSseEmitter(
+        @AuthenticationPrincipal principal: UserPrincipal
+    ): SseEmitter {
+        val emitter = sseService.findOrCreateEmitter(principal.id)
+        emitter.send(SseEmitter.event().name("connect").data(""))
+        return emitter
+    }
+
+}

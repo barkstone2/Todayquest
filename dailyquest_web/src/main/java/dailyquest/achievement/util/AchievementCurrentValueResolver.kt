f8808caa1ea22f49044dc83c304209192c5fc16a
barkstone <barkstone2@gmail.com>
2024-03-14 17:27:50 +0900

    refactor: 업적 현재값 조회기 인터페이스로 추상화

--- dailyquest_web/src/main/java/dailyquest/achievement/util/AchievementCurrentValueResolver.kt
+++ dailyquest_web/src/main/java/dailyquest/achievement/util/AchievementCurrentValueResolver.kt
@@ -2,30 +2,10 @@ package dailyquest.achievement.util
 
 import dailyquest.achievement.dto.AchievementAchieveRequest
 import dailyquest.achievement.entity.Achievement
-import dailyquest.achievement.entity.AchievementType
-import dailyquest.log.gold.earn.service.GoldEarnLogService
-import dailyquest.quest.service.QuestLogService
-import dailyquest.user.service.UserService
-import org.springframework.stereotype.Component
 
-@Component
-class AchievementCurrentValueResolver(
-    private val questLogService: QuestLogService,
-    private val userService: UserService,
-    private val goldEarnLogService: GoldEarnLogService
-) {
+interface AchievementCurrentValueResolver {
     fun resolveCurrentValue(
         achieveRequest: AchievementAchieveRequest,
         targetAchievement: Achievement
-    ): Int {
-        return when (achieveRequest.type) {
-            AchievementType.QUEST_REGISTRATION -> questLogService.getTotalRegistrationCount(achieveRequest.userId)
-            AchievementType.QUEST_COMPLETION -> questLogService.getTotalCompletionCount(achieveRequest.userId)
-            AchievementType.QUEST_CONTINUOUS_REGISTRATION_DAYS -> questLogService.getRegistrationDaysSince(achieveRequest.userId, targetAchievement.targetValue)
-            AchievementType.QUEST_CONTINUOUS_COMPLETION -> questLogService.getCompletionDaysSince(achieveRequest.userId, targetAchievement.targetValue)
-            AchievementType.USER_LEVEL -> userService.getUserPrincipal(achieveRequest.userId).level
-            AchievementType.GOLD_EARN -> goldEarnLogService.getTotalGoldEarnOfUser(achieveRequest.userId)
-            AchievementType.EMPTY -> 0
-        }
-    }
+    ): Int
 }

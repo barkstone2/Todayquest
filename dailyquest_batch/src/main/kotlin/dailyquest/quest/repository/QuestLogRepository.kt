a9356447939fd5f6831e6dd32fe50a97378bde60
barkstone <barkstone2@gmail.com>
2024-03-18 20:12:30 +0900

    feat: 배치에 완벽한 하루를 달성한 유저 ID 조회 API 추가

--- dailyquest_batch/src/main/kotlin/dailyquest/quest/repository/QuestLogRepository.kt
+++ dailyquest_batch/src/main/kotlin/dailyquest/quest/repository/QuestLogRepository.kt
@@ -2,5 +2,22 @@ package dailyquest.quest.repository
 
 import dailyquest.quest.entity.QuestLog
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 
-interface QuestLogRepository : JpaRepository<QuestLog, Long>
+interface QuestLogRepository : JpaRepository<QuestLog, Long> {
+
+    @Query("select qlp.userId " +
+            "from QuestLog qlp " +
+            "left join QuestLog qlc " +
+                "on qlc.loggedDate = :loggedDate " +
+                "and qlc.state = 'COMPLETE' " +
+                "and qlc.userId = qlp.userId " +
+                "and qlc.questId = qlp.questId " +
+            "where qlp.loggedDate = :loggedDate " +
+            "and qlp.state = 'PROCEED' " +
+            "group by qlp.userId " +
+            "having count(qlp.questId) = count(qlc.questId)")
+    fun getAllUserIdsWhoAchievedPerfectDay(@Param("loggedDate") loggedDate: LocalDate): List<Long>
+}

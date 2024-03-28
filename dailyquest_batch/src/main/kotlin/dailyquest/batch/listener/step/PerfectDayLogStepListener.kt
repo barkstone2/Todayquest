7df463335e70f21d34b57ae135ac87f49727c4e4
barkstone <barkstone2@gmail.com>
2024-03-28 15:48:14 +0900

    fix: 완벽한 하루 스텝 리스너 오류 수정
    - 테스트 코드를 기반으로 컨텍스트 처리 오류 수정

--- dailyquest_batch/src/main/kotlin/dailyquest/batch/listener/step/PerfectDayLogStepListener.kt
+++ dailyquest_batch/src/main/kotlin/dailyquest/batch/listener/step/PerfectDayLogStepListener.kt
@@ -1,11 +1,15 @@
 package dailyquest.batch.listener.step
 
-import dailyquest.achievement.entity.AchievementAchieveLog
 import dailyquest.common.util.ExecutionContextUtil
+import dailyquest.log.perfectday.entity.PerfectDayLog
+import org.springframework.batch.core.ExitStatus
 import org.springframework.batch.core.StepExecution
+import org.springframework.batch.core.annotation.AfterChunk
+import org.springframework.batch.core.annotation.AfterStep
 import org.springframework.batch.core.annotation.AfterWrite
 import org.springframework.batch.core.annotation.BeforeStep
 import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.item.Chunk
 import org.springframework.stereotype.Component
 
@@ -20,15 +24,23 @@ class PerfectDayLogStepListener {
         executionContextUtil = ExecutionContextUtil.from(stepExecution)
     }
 
-    @AfterWrite
-    fun afterWrite(chunk: Chunk<AchievementAchieveLog>) {
-        val newUserIds = chunk.map { it.userId }.toList()
-        val userIds = this.getUserIdsFromJobContext()
-        userIds.addAll(newUserIds)
-        executionContextUtil.putToJobContext(userIdsKey, userIds)
+    @AfterStep
+    fun afterStep(stepExecution: StepExecution): ExitStatus {
+        executionContextUtil.removeFromStepContext(userIdsKey)
+        return stepExecution.exitStatus
     }
 
-    private fun getUserIdsFromJobContext(): MutableList<Long> {
-        return executionContextUtil.getFromJobContext(userIdsKey) ?: mutableListOf()
+    @AfterWrite
+    fun afterWrite(chunk: Chunk<PerfectDayLog>) {
+        val userIdsToCommit = chunk.map { it.userId }.toList()
+        executionContextUtil.putToStepContext(userIdsKey, userIdsToCommit)
+    }
+
+    @AfterChunk
+    fun afterChunk(context: ChunkContext) {
+        if (context.isComplete) {
+            executionContextUtil.mergeListFromStepContextToJobContext<Long>(userIdsKey)
+        }
+        executionContextUtil.removeFromStepContext(userIdsKey)
     }
 }

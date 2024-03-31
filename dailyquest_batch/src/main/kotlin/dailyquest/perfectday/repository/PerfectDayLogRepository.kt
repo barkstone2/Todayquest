04d2f92d6147d5a1ad8d30cdf6323088aaab90e0
barkstone <barkstone2@gmail.com>
2024-03-31 17:02:29 +0900

    test: 완벽한 하루 업적 스텝 유닛 테스트 작성
    - 완벽한 하루 로그 리포지토리 조회 메서드에 페이징 추가
    - 완벽한 하루 업적 스텝 리더에 누락된 설정 추가
    - reader, processor, writer, listener 유닛 테스트 추가
    - Achievement, AchievementAchieveLog 엔티티에 Serializable 추가
    - 리스너 오류 수정
    - 완벽한 하루 Job 설정 오타 수정

--- dailyquest_batch/src/main/kotlin/dailyquest/perfectday/repository/PerfectDayLogRepository.kt
+++ dailyquest_batch/src/main/kotlin/dailyquest/perfectday/repository/PerfectDayLogRepository.kt
@@ -2,11 +2,13 @@ package dailyquest.perfectday.repository
 
 import dailyquest.log.perfectday.entity.PerfectDayLog
 import dailyquest.perfectday.dto.PerfectDayCount
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface PerfectDayLogRepository: JpaRepository<PerfectDayLog, Long> {
     @Query("select new dailyquest.perfectday.dto.PerfectDayCount(pdl.userId, count(pdl.loggedDate)) from PerfectDayLog pdl where pdl.userId in (:userIds) group by pdl.userId")
-    fun countByUserIds(@Param("userIds") userIds: List<Long>): List<PerfectDayCount>
+    fun countByUserIds(@Param("userIds") userIds: List<Long>, pageable: Pageable): Page<PerfectDayCount>
 }

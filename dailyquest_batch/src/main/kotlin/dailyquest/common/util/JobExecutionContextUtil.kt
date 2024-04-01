4b9c581ab316550dc392ee365191b9499d99a979
barkstone <barkstone2@gmail.com>
2024-04-01 13:18:06 +0900

    feat: ExecutionContextUtil 타입 인자 수정 및 null 처리
    - 유닛 테스트 추가
    - JobExecutionContextUtil에 정적 팩토리 메서드 추가

--- dailyquest_batch/src/main/kotlin/dailyquest/common/util/JobExecutionContextUtil.kt
+++ dailyquest_batch/src/main/kotlin/dailyquest/common/util/JobExecutionContextUtil.kt
@@ -2,15 +2,24 @@ package dailyquest.common.util
 
 import org.springframework.batch.core.JobExecution
 
-class JobExecutionContextUtil(
+@Suppress("UNCHECKED_CAST")
+class JobExecutionContextUtil private constructor(
     private val jobExecution: JobExecution
 ) {
-    @Suppress("UNCHECKED_CAST")
-    fun <T> getFromJobContext(key: String): T {
-        return jobExecution.executionContext.get(key) as T
+    @Throws(ClassCastException::class)
+    fun <T> getFromJobContext(key: String): T? {
+        return jobExecution.executionContext.get(key) as? T
     }
 
     fun putToJobContext(key: String, value: Any) {
         jobExecution.executionContext.put(key, value)
+    }
+
+    companion object {
+        @JvmStatic
+        fun from(jobExecution: JobExecution): JobExecutionContextUtil {
+            val executionContextUtil = JobExecutionContextUtil(jobExecution)
+            return executionContextUtil
+        }
     }
 }

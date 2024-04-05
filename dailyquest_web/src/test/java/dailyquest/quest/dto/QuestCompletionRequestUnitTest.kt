295c585954405aa8cd69cabeffc8acb56ef75b08
barkstone <barkstone2@gmail.com>
2024-04-05 16:39:35 +0900

    feat: 퀘스트 완료 처리 로직 수정
    - 유저 경험치, 골드 획득 처리를 위한 서브 타입 DTO 추가

---
+++ dailyquest_web/src/test/java/dailyquest/quest/dto/QuestCompletionRequestUnitTest.kt
@@ -0,0 +1,79 @@
+package dailyquest.quest.dto
+
+import dailyquest.quest.entity.QuestType
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("퀘스트 완료 요청 DTO 유닛 테스트")
+class QuestCompletionRequestUnitTest {
+
+    @DisplayName("획득한 경험치 요청 시")
+    @Nested
+    inner class TestCalculateExp {
+        @DisplayName("현재 타입이 main이면 2배의 경험치가 반환된다")
+        @Test
+        fun `현재 타입이 main이면 2배의 경험치가 반환된다`() {
+            //given
+            val baseExp = 10L
+            val type = QuestType.MAIN
+            val request = QuestCompletionRequest(earnedExp = baseExp, questId = 1, type = type)
+
+            //when
+            val resultExp = request.earnedExp
+
+            //then
+            Assertions.assertThat(resultExp).isEqualTo(baseExp * 2)
+        }
+
+        @DisplayName("현재 타입이 sub면 1배의 경험치가 반환된다")
+        @Test
+        fun `현재 타입이 sub면 1배의 경험치가 반환된다`() {
+            //given
+            val baseExp = 10L
+            val type = QuestType.SUB
+            val request = QuestCompletionRequest(earnedExp = baseExp, questId = 1, type = type)
+
+            //when
+            val resultExp = request.earnedExp
+
+            //then
+            Assertions.assertThat(resultExp).isEqualTo(baseExp * 1)
+        }
+    }
+
+    @DisplayName("획득한 골드 요청 시")
+    @Nested
+    inner class TestCalculateGold {
+        @DisplayName("현재 타입이 main이면 2배의 골드가 반환된다")
+        @Test
+        fun `현재 타입이 main이면 2배의 골드가 반환된다`() {
+            //given
+            val baseGold = 10L
+            val type = QuestType.MAIN
+            val request = QuestCompletionRequest(earnedGold = baseGold, questId = 1, type = type)
+
+            //when
+            val resultExp = request.earnedGold
+
+            //then
+            Assertions.assertThat(resultExp).isEqualTo(baseGold * 2)
+        }
+
+        @DisplayName("현재 타입이 sub면 1배의 골드가 반환된다")
+        @Test
+        fun `현재 타입이 sub면 1배의 골드가 반환된다`() {
+            //given
+            val baseGold = 10L
+            val type = QuestType.SUB
+            val request = QuestCompletionRequest(earnedGold = baseGold, questId = 1, type = type)
+
+            //when
+            val resultExp = request.earnedGold
+
+            //then
+            Assertions.assertThat(resultExp).isEqualTo(baseGold * 1)
+        }
+    }
+}

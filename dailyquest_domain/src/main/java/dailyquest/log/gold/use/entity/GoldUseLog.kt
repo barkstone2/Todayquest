1080bcc0ec29037ed9a4fe3609904b9768365085
barkstone <barkstone2@gmail.com>
2024-04-05 15:52:40 +0900

    refactor: 도메인 모듈에서 유저 모듈로 분리해낸 클래스들 제거
    - 도메인 모듈 내부에서 유저 모듈의 클래스 참조하도록 변경

--- dailyquest_domain/src/main/java/dailyquest/log/gold/use/entity/GoldUseLog.kt
+++ dailyquest_domain/src/main/java/dailyquest/log/gold/use/entity/GoldUseLog.kt
@@ -1,24 +1,23 @@
 package dailyquest.log.gold.use.entity
 
 import dailyquest.common.CreatedTimeEntity
-import dailyquest.user.entity.UserInfo
+import dailyquest.user.entity.User
 import jakarta.persistence.*
 
 @Table(name = "gold_use_log")
 @Entity
 class GoldUseLog(
-    user: UserInfo,
+    user: User,
     amount: Int,
     source: GoldUseSource
 ): CreatedTimeEntity() {
     @Column(name = "gold_use_log_id")
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    val user: UserInfo = user
+    val user: User = user
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)

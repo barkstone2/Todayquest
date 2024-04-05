faaa21cb2b17fa5c5734ed98a91ccbc1579417ad
barkstone <barkstone2@gmail.com>
2024-04-05 16:24:12 +0900

    feat: UserPrincipal에 유저 엔티티에 추가된 정보 추가

--- dailyquest_web/src/main/java/dailyquest/user/dto/UserPrincipal.kt
+++ dailyquest_web/src/main/java/dailyquest/user/dto/UserPrincipal.kt
@@ -3,25 +3,39 @@ package dailyquest.user.dto
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonGetter
 import com.fasterxml.jackson.annotation.JsonIgnore
+import dailyquest.user.entity.ProviderType
+import dailyquest.user.entity.RoleType
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
-import dailyquest.user.entity.ProviderType
-import dailyquest.user.entity.UserInfo
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class UserPrincipal(
     val id: Long,
-    var nickname: String,
-    val providerType: ProviderType,
-    private var authorities: MutableCollection<GrantedAuthority>,
-    var level: Int,
-    var currentExp: Long,
-    var requireExp: Long,
-    var gold: Long,
-    var coreTime: Int,
+    val nickname: String,
+    val providerType: ProviderType = ProviderType.GOOGLE,
+    private val authorities: MutableCollection<GrantedAuthority> = mutableListOf(SimpleGrantedAuthority(RoleType.USER.code)),
+    val level: Int = 0,
+    val currentExp: Long = 0,
+    val requireExp: Long = 0,
+    val gold: Long = 0,
+    val coreTime: Int = 8,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    var coreTimeLastModifiedDate: LocalDateTime? = null
+    val coreTimeLastModifiedDate: LocalDateTime? = null,
+    val questRegistrationCount: Int = 0,
+    val questCompletionCount: Int = 0,
+    val currentQuestContinuousRegistrationDays: Int = 0,
+    val currentQuestContinuousCompletionDays: Int = 0,
+    val maxQuestContinuousRegistrationDays: Int = 0,
+    val maxQuestContinuousCompletionDays: Int = 0,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    val lastQuestRegistrationDate: LocalDate? = null,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    val lastQuestCompletionDate: LocalDate? = null,
+    val perfectDayCount: Int = 0,
+    val goldEarnAmount: Int = 0,
+    val goldUseAmount: Int = 0,
 ) : UserDetails {
 
     @JsonIgnore
@@ -60,21 +74,30 @@ class UserPrincipal(
 
     companion object {
         @JvmStatic
-        fun create(userInfo: UserInfo, expTable: Map<Int, Long>): UserPrincipal {
-
-            val (currentLevel, currentExp, requireExp) = userInfo.calculateLevel(expTable)
-
+        fun from(userResponse: UserResponse, expTable: Map<Int, Long>): UserPrincipal {
+            val (currentLevel, currentExp, requireExp) = userResponse.calculateLevel(expTable)
             return UserPrincipal(
-                id = userInfo.id,
-                nickname = userInfo.nickname,
-                providerType = userInfo.providerType,
-                authorities = mutableListOf(SimpleGrantedAuthority(userInfo.role.code)),
+                id = userResponse.id,
+                nickname = userResponse.nickname,
+                userResponse.providerType,
+                authorities = mutableListOf(SimpleGrantedAuthority(userResponse.role.code)),
                 level = currentLevel,
                 currentExp = currentExp,
                 requireExp = requireExp,
-                gold = userInfo.gold,
-                coreTime = userInfo.getCoreHour(),
-                coreTimeLastModifiedDate = userInfo.coreTimeLastModifiedDate
+                gold = userResponse.gold,
+                coreTime = userResponse.coreTime.hour,
+                coreTimeLastModifiedDate = userResponse.coreTimeLastModifiedDate,
+                questRegistrationCount = userResponse.questRegistrationCount,
+                questCompletionCount = userResponse.questCompletionCount,
+                currentQuestContinuousRegistrationDays = userResponse.currentQuestContinuousRegistrationDays,
+                currentQuestContinuousCompletionDays = userResponse.currentQuestContinuousCompletionDays,
+                maxQuestContinuousRegistrationDays = userResponse.maxQuestContinuousRegistrationDays,
+                maxQuestContinuousCompletionDays = userResponse.maxQuestContinuousCompletionDays,
+                lastQuestRegistrationDate = userResponse.lastQuestRegistrationDate,
+                lastQuestCompletionDate = userResponse.lastQuestCompletionDate,
+                perfectDayCount = userResponse.perfectDayCount,
+                goldEarnAmount = userResponse.goldEarnAmount,
+                goldUseAmount = userResponse.goldUseAmount,
             )
         }
     }

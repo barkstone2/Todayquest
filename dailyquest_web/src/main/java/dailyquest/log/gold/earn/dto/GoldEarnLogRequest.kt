7152b57de469d3a785657d0b91416d5a4b062dd2
barkstone <barkstone2@gmail.com>
2024-04-05 15:54:29 +0900

    remove: 웹 모듈에서 유저 모듈로 분리해낸 클래스들 제거

--- dailyquest_web/src/main/java/dailyquest/log/gold/earn/dto/GoldEarnLogRequest.kt
+++
@@ -1,9 +0,0 @@
-package dailyquest.log.gold.earn.dto
-
-import dailyquest.log.gold.earn.entity.GoldEarnSource
-
-class GoldEarnLogRequest(
-    val amount: Long,
-    val source: GoldEarnSource
-) {
-}

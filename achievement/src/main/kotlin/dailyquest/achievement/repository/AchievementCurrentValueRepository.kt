42d827c80d7d021acf6d5f1fe306119fd03ea660
barkstone <barkstone2@gmail.com>
2024-04-05 17:13:28 +0900

    feat: 업적 현재값 엔티티 제거하고 요청 DTO에 현재값 담도록 변경

--- achievement/src/main/kotlin/dailyquest/achievement/repository/AchievementCurrentValueRepository.kt
+++
@@ -1,7 +0,0 @@
-package dailyquest.achievement.repository
-
-import dailyquest.achievement.entity.AchievementCurrentValue
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AchievementCurrentValueRepository: JpaRepository<AchievementCurrentValue, Long> {
-}

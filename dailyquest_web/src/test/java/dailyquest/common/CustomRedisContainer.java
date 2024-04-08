02713d2190ba2d3307859230991bf260363b99c4
barkstone <barkstone2@gmail.com>
2024-04-08 18:58:53 +0900

    test: 테스트 수정 및 오타 수정
    - 배치 테스트 오류 수정
    - 유저 테스트용 클래스 추가
    - 업적 관리자 컨트롤러 테스트 추가
    - 레디스 컨테이너 초기화 스크립트 수정

--- dailyquest_web/src/test/java/dailyquest/common/CustomRedisContainer.java
+++ dailyquest_web/src/test/java/dailyquest/common/CustomRedisContainer.java
@@ -5,11 +5,8 @@ import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CustomRedisContainer extends RedisContainer {
-
-    private final AtomicBoolean initialized = new AtomicBoolean(false);
 
     public CustomRedisContainer() {
         super(DockerImageName.parse("redis:latest"));
@@ -18,8 +15,6 @@ public class CustomRedisContainer extends RedisContainer {
     }
 
     public void initRedis() throws IOException, InterruptedException {
-        if(initialized.get()) return;
-        initialized.set(true);
         this.execInContainer("/data/redis_init.sh");
     }
 }

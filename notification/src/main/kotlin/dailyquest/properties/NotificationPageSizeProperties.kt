63eabfeabcc2ab069981b842f3a61a1dfc9dbccd
barkstone <barkstone2@gmail.com>
2024-04-11 13:51:38 +0900

    feat: 알림 페이지 사이즈 프로퍼티 클래스 기본값 추가
    - 웹 모듈에서 중복된 프로퍼티 클래스 제거
    - 프로퍼티 주 생성자로 이동
    - 단위 테스트 추가

--- notification/src/main/kotlin/dailyquest/properties/NotificationPageSizeProperties.kt
+++ notification/src/main/kotlin/dailyquest/properties/NotificationPageSizeProperties.kt
@@ -1,10 +1,8 @@
 package dailyquest.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
-@Component
 @ConfigurationProperties(prefix = "notification.page")
-class NotificationPageSizeProperties {
-    var size = 0
-}
+class NotificationPageSizeProperties(
+    val size: Int = 10
+)

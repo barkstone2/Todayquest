9c6a0fc1bb23057074afa18e2f7c87fd876f5bf8
barkstone <barkstone2@gmail.com>
2024-04-11 19:35:41 +0900

    feat: 프로퍼티 맵 생성 로직 리플렉션 유틸로 이동

--- reflection-common/src/main/kotlin/dailyquest/common/ReflectionUtil.kt
+++ reflection-common/src/main/kotlin/dailyquest/common/ReflectionUtil.kt
@@ -8,7 +8,14 @@ import kotlin.reflect.full.memberProperties
 class ReflectionUtil {
     companion object {
         @JvmStatic
-        fun <T : Any> getAnnotatedProperties(targetClass: KClass<T>, targetAnnotation: KClass<out Annotation>): List<KProperty1<T, *>> {
+        fun <T : Any> getAnnotatedPropertiesMap(instance: T, targetAnnotation: KClass<out Annotation>): Map<String, Any> {
+            val result = mutableMapOf<String, Any>()
+            val annotatedProperties = this.getAnnotatedProperties(instance.javaClass.kotlin, targetAnnotation)
+            annotatedProperties.forEach { result[it.name] = it.get(instance).toString() }
+            return result
+        }
+
+        private fun <T : Any> getAnnotatedProperties(targetClass: KClass<T>, targetAnnotation: KClass<out Annotation>): List<KProperty1<T, *>> {
             val annotatedProperties = targetClass.memberProperties.filter { it.findAnnotations(targetAnnotation).isNotEmpty() }
             return annotatedProperties
         }

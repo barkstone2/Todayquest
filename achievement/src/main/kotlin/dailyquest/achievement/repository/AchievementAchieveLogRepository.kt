2fa526f5d70d581ff46e282358b3716f3fdbbfb1
barkstone <barkstone2@gmail.com>
2024-04-11 21:21:58 +0900

    test: 업적 모듈 단위 테스트 추가

--- achievement/src/main/kotlin/dailyquest/achievement/repository/AchievementAchieveLogRepository.kt
+++ achievement/src/main/kotlin/dailyquest/achievement/repository/AchievementAchieveLogRepository.kt
@@ -3,5 +3,4 @@ package dailyquest.achievement.repository
 import dailyquest.achievement.entity.AchievementAchieveLog
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AchievementAchieveLogRepository: JpaRepository<AchievementAchieveLog, Long> {
-}
+interface AchievementAchieveLogRepository: JpaRepository<AchievementAchieveLog, Long>

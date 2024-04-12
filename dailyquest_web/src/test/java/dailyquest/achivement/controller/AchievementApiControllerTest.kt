87376938a767d247bdd18669ed28f24bb20805f4
barkstone <barkstone2@gmail.com>
2024-04-12 15:54:46 +0900

    test: 업적 컨트롤러 통합 테스트 추가
    - 통합 테스트 컨텍스트 클래스에 요청 DSL에 대한 확장함수 추가

---
+++ dailyquest_web/src/test/java/dailyquest/achivement/controller/AchievementApiControllerTest.kt
@@ -0,0 +1,356 @@
+package dailyquest.achivement.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.SpykBean
+import dailyquest.achievement.entity.Achievement
+import dailyquest.achievement.entity.AchievementAchieveLog
+import dailyquest.achievement.entity.AchievementType
+import dailyquest.achievement.repository.AchievementAchieveLogRepository
+import dailyquest.achievement.repository.AchievementRepository
+import dailyquest.achievement.service.AchievementService
+import dailyquest.context.IntegrationTestContext
+import dailyquest.context.MockElasticsearchTestContextConfig
+import dailyquest.context.MockRedisTestContextConfig
+import dailyquest.jwt.JwtTokenProvider
+import dailyquest.properties.AchievementPageSizeProperties
+import dailyquest.user.repository.UserRepository
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.web.servlet.get
+import org.springframework.web.context.WebApplicationContext
+
+@Import(MockElasticsearchTestContextConfig::class, MockRedisTestContextConfig::class)
+@ExtendWith(MockKExtension::class)
+@DisplayName("업적 API 컨트롤러 통합 테스트")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AchievementApiControllerTest @Autowired constructor(
+    context: WebApplicationContext,
+    userRepository: UserRepository,
+    jwtTokenProvider: JwtTokenProvider,
+    private val achievementService: AchievementService,
+    private val achievementRepository: AchievementRepository,
+    private val achievementAchieveLogRepository: AchievementAchieveLogRepository,
+    private val om: ObjectMapper,
+    @SpykBean
+    private val achievementPageSizeProperties: AchievementPageSizeProperties
+) : IntegrationTestContext(context, userRepository, jwtTokenProvider) {
+    private val uriPrefix = "/api/v1/achievements"
+
+
+    @DisplayName("달성한 업적 조회 시")
+    @Nested
+    inner class TestGetAchievedAchievements {
+        private val url = "$uriPrefix/achieved"
+
+        @DisplayName("달성하지 않은 업적은 조회되지 않는다")
+        @Test
+        fun `달성하지 않은 업적은 조회되지 않는다`() {
+            //given
+            val notAchievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(notAchievedAchievement)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(not(contains(notAchievedAchievement.id.toInt())))
+                }
+            }
+        }
+
+        @DisplayName("다른 유저가 달성한 업적은 조회되지 않는다")
+        @Test
+        fun `다른 유저가 달성한 업적은 조회되지 않는다`() {
+            //given
+            val anotherUserId = anotherUser.id
+            val anotherUserAchievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(anotherUserAchievedAchievement)
+            val achievementAchieveLog = AchievementAchieveLog(anotherUserAchievedAchievement, anotherUserId)
+            achievementAchieveLogRepository.save(achievementAchieveLog)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(not(contains(anotherUserAchievedAchievement.id.toInt())))
+                }
+            }
+        }
+
+        @DisplayName("현재 유저가 달성한 업적이 조회된다")
+        @Test
+        fun `현재 유저가 달성한 업적이 조회된다`() {
+            //given
+            val userId = user.id
+            val achievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(achievedAchievement)
+            val achievementAchieveLog = AchievementAchieveLog(achievedAchievement, userId)
+            achievementAchieveLogRepository.save(achievementAchieveLog)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(contains(achievedAchievement.id.toInt()))
+                }
+            }
+        }
+
+        @DisplayName("요청한 페이지의 데이터만 조회된다")
+        @Test
+        fun `요청한 페이지의 데이터만 조회된다`() {
+            //given
+            val pageNo = 0
+            val idx = 2 - pageNo
+            val userId = user.id
+            val achievements = listOf(
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 2)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 3)),
+            )
+            achievements.forEach {
+                achievementAchieveLogRepository.save(AchievementAchieveLog(it, userId))
+            }
+            every { achievementPageSizeProperties.size } returns 1
+
+            //when
+            val result = mvc.get("$url?page=$pageNo") {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.size()") { value(1) }
+                jsonPath("$.data.content.*.id") {
+                    value(contains(achievements[idx].id.toInt()))
+                }
+            }
+        }
+
+        @DisplayName("비활성화 상태의 업적은 조회되지 않는다")
+        @Test
+        fun `비활성화 상태의 업적은 조회되지 않는다`() {
+            //given
+            val userId = user.id
+            val inactivatedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            inactivatedAchievement.inactivateAchievement()
+            achievementRepository.save(inactivatedAchievement)
+            val achievementAchieveLog = AchievementAchieveLog(inactivatedAchievement, userId)
+            achievementAchieveLogRepository.save(achievementAchieveLog)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(not(contains(inactivatedAchievement.id.toInt())))
+                }
+            }
+        }
+
+        @DisplayName("업적 달성일이 최근인 순으로 조회된다")
+        @Test
+        fun `업적 달성일이 최근인 순으로 조회된다`() {
+            //given
+            val userId = user.id
+            val achievements = listOf(
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 2)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 3)),
+            )
+            achievements.forEach {
+                achievementAchieveLogRepository.save(AchievementAchieveLog(it, userId))
+            }
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(containsInRelativeOrder(achievements.reversed().map { it.id.toInt() }.reduce { _, i -> i }))
+                }
+            }
+        }
+    }
+
+    @DisplayName("달성하지 못한 업적 조회 시")
+    @Nested
+    inner class TestGetNotAchievedAchievements {
+        private val url = "$uriPrefix/not-achieved"
+
+        @DisplayName("달성한 업적은 조회되지 않는다")
+        @Test
+        fun `달성한 업적은 조회되지 않는다`() {
+            //given
+            val userId = user.id
+            val achievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(achievedAchievement)
+            val achievementAchieveLog = AchievementAchieveLog(achievedAchievement, userId)
+            achievementAchieveLogRepository.save(achievementAchieveLog)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(not(contains(achievedAchievement.id.toInt())))
+                }
+            }
+        }
+
+        @DisplayName("다른 유저가 달성한 업적은 조회된다")
+        @Test
+        fun `다른 유저가 달성한 업적은 조회된다`() {
+            //given
+            val anotherUserId = anotherUser.id
+            val anotherUserAchievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(anotherUserAchievedAchievement)
+            val achievementAchieveLog = AchievementAchieveLog(anotherUserAchievedAchievement, anotherUserId)
+            achievementAchieveLogRepository.save(achievementAchieveLog)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(contains(anotherUserAchievedAchievement.id.toInt()))
+                }
+            }
+        }
+
+        @DisplayName("현재 유저가 달성하지 않은 업적이 조회된다")
+        @Test
+        fun `현재 유저가 달성하지 않은 업적이 조회된다`() {
+            //given
+            val notAchievedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(notAchievedAchievement)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(contains(notAchievedAchievement.id.toInt()))
+                }
+            }
+        }
+
+        @DisplayName("요청한 페이지의 데이터만 조회된다")
+        @Test
+        fun `요청한 페이지의 데이터만 조회된다`() {
+            //given
+            val pageNo = 0
+            val achievements = listOf(
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 2)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 3)),
+            )
+            every { achievementPageSizeProperties.size } returns 1
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+                param("page", pageNo.toString())
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.size()") { value(1) }
+                jsonPath("$.data.content.*.id") {
+                    value(contains(achievements[pageNo].id.toInt()))
+                }
+            }
+        }
+
+        @DisplayName("비활성화 상태의 업적은 조회되지 않는다")
+        @Test
+        fun `비활성화 상태의 업적은 조회되지 않는다`() {
+            //given
+            val inactivatedAchievement = Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)
+            inactivatedAchievement.inactivateAchievement()
+            achievementRepository.save(inactivatedAchievement)
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(not(contains(inactivatedAchievement.id.toInt())))
+                }
+            }
+        }
+
+        @DisplayName("목표값이 낮은 순으로 조회된다")
+        @Test
+        fun `목표값이 낮은 순으로 조회된다`() {
+            //given
+            val achievements = listOf(
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 1)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 2)),
+                achievementRepository.save(Achievement("t", "d", AchievementType.QUEST_REGISTRATION, 3)),
+            )
+
+            //when
+            val result = mvc.get(url) {
+                useUserConfiguration()
+            }
+
+            //then
+            result.andExpect {
+                status { isOk() }
+                jsonPath("$.data.content.*.id") {
+                    value(containsInRelativeOrder(achievements.map { it.id.toInt() }.reduce { _, i -> i }))
+                }
+            }
+        }
+    }
+}
--- dailyquest_web/src/test/java/dailyquest/achivement/controller/AdminAchievementApiControllerTest.kt
+++ dailyquest_web/src/test/java/dailyquest/achivement/controller/AdminAchievementApiControllerTest.kt
@@ -2,6 +2,8 @@ package dailyquest.achivement.controller
 
 import com.ninjasquad.springmockk.MockkBean
 import dailyquest.achievement.dto.AchievementSaveRequest
+import dailyquest.achievement.dto.AchievementUpdateRequest
+import dailyquest.achievement.entity.Achievement
 import dailyquest.achievement.entity.AchievementType
 import dailyquest.achievement.repository.AchievementRepository
 import dailyquest.common.BatchApiUtil
@@ -11,7 +13,9 @@ import dailyquest.context.MockRedisTestContextConfig
 import dailyquest.jwt.JwtTokenProvider
 import dailyquest.user.repository.UserRepository
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -19,6 +23,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.web.servlet.patch
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.web.context.WebApplicationContext
@@ -101,6 +107,135 @@ class AdminAchievementApiControllerTest @Autowired constructor(
             //then
             val allAchievements = achievementRepository.findAll()
             assertThat(allAchievements).anyMatch { it.type == saveRequest.type && it.targetValue == saveRequest.targetValue }
+        }
+
+        @DisplayName("신규 업적 등록 후 배치에 업적 달성 확인 요청을 보낸다")
+        @Test
+        fun `신규 업적 등록 후 배치에 업적 달성 확인 요청을 보낸다`() {
+            //given
+            val requestBody = om.writeValueAsString(saveRequest)
+
+            //when
+            mvc.perform {
+                MockMvcRequestBuilders.post(url)
+                    .useAdminConfiguration()
+                    .content(requestBody)
+                    .buildRequest(it)
+            }
+
+            //then
+            verify { batchApiUtil.checkAndAchieve(any()) }
+        }
+    }
+
+    @DisplayName("업적 변경 요청 시")
+    @Nested
+    inner class TestUpdateAchievement {
+        private lateinit var achievement: Achievement
+
+        @BeforeEach
+        fun init() {
+            achievement = Achievement("save", "save", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(achievement)
+        }
+
+        @DisplayName("업적의 제목이 요청한 제목으로 변경된다")
+        @Test
+        fun `업적의 제목이 요청한 제목으로 변경된다`() {
+            //given
+            val url = "$uriPrefix/${achievement.id}"
+            val updateTitle = "update"
+            val updateRequest = AchievementUpdateRequest(updateTitle, "update")
+            val requestBody = om.writeValueAsString(updateRequest)
+
+            //when
+            mvc.patch(url) {
+                useAdminConfiguration()
+                content = requestBody
+            }
+
+            //then
+            val result = achievementRepository.findByIdOrNull(achievement.id)
+            assertThat(result?.title).isEqualTo(updateTitle)
+        }
+
+        @DisplayName("업적의 설명이 요청한 설명으로 변경된다")
+        @Test
+        fun `업적의 설명이 요청한 설명으로 변경된다`() {
+            //given
+            val url = "$uriPrefix/${achievement.id}"
+            val updateDescription = "update"
+            val updateRequest = AchievementUpdateRequest("update", updateDescription)
+            val requestBody = om.writeValueAsString(updateRequest)
+
+            //when
+            mvc.patch(url) {
+                useAdminConfiguration()
+                content = requestBody
+            }
+
+            //then
+            val result = achievementRepository.findByIdOrNull(achievement.id)
+            assertThat(result?.description).isEqualTo(updateDescription)
+        }
+    }
+
+    @DisplayName("업적 비활성화 요청 시")
+    @Nested
+    inner class TestInactivateAchievement {
+        private lateinit var achievement: Achievement
+
+        @BeforeEach
+        fun init() {
+            achievement = Achievement("save", "save", AchievementType.QUEST_REGISTRATION, 1)
+            achievementRepository.save(achievement)
+        }
+
+        @DisplayName("업적이 비활성화 상태로 변경된다")
+        @Test
+        fun `업적이 비활성화 상태로 변경된다`() {
+            //given
+            val beforeState = achievement.inactivated
+            val url = "$uriPrefix/${achievement.id}/inactivate"
+
+            //when
+            mvc.patch(url) {
+                useAdminConfiguration()
+            }
+
+            //then
+            val result = achievementRepository.findByIdOrNull(achievement.id)
+            assertThat(result?.inactivated).isNotEqualTo(beforeState).isTrue()
+        }
+    }
+
+    @DisplayName("업적 활성화 요청 시")
+    @Nested
+    inner class TestActivateAchievement {
+        private lateinit var achievement: Achievement
+
+        @BeforeEach
+        fun init() {
+            achievement = Achievement("save", "save", AchievementType.QUEST_REGISTRATION, 1)
+            achievement.inactivateAchievement()
+            achievementRepository.save(achievement)
+        }
+
+        @DisplayName("업적이 활성화 상태로 변경된다")
+        @Test
+        fun `업적이 활성화 상태로 변경된다`() {
+            //given
+            val beforeState = achievement.inactivated
+            val url = "$uriPrefix/${achievement.id}/activate"
+
+            //when
+            mvc.patch(url) {
+                useAdminConfiguration()
+            }
+
+            //then
+            val result = achievementRepository.findByIdOrNull(achievement.id)
+            assertThat(result?.inactivated).isNotEqualTo(beforeState).isFalse()
         }
     }
 }

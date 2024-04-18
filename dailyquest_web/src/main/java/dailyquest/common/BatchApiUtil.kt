26aa764b5b17c1f0f5c4ecc6b1d17343ac95e4f4
barkstone <barkstone2@gmail.com>
2024-04-18 17:31:23 +0900

    feat: 업적 신규 등록 후 배치 작업 요청 시 비동기로 실행
    - 비동기 작업 실패시 로깅

--- dailyquest_web/src/main/java/dailyquest/common/BatchApiUtil.kt
+++ dailyquest_web/src/main/java/dailyquest/common/BatchApiUtil.kt
@@ -1,6 +1,9 @@
 package dailyquest.common
 
 import dailyquest.properties.BatchApiProperties
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
@@ -9,7 +12,14 @@ class BatchApiUtil(
     private val batchRestTemplate: RestTemplate,
     private val batchApiProperties: BatchApiProperties
 ) {
+    private val logger: Logger = LoggerFactory.getLogger("batchErrorLogger")
+
+    @Async
     fun checkAndAchieve(achievementId: Long) {
-        batchRestTemplate.postForEntity(batchApiProperties.getCheckAndAchieveUrl(), achievementId, Void::class.java)
+        try {
+            batchRestTemplate.postForEntity(batchApiProperties.getCheckAndAchieveUrl(), achievementId, Void::class.java)
+        } catch (ex: Exception) {
+            logger.error("checkAndAchieve request for $achievementId is failed.")
+        }
     }
 }

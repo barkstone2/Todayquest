016e79e9893d51108f2fe6486a576c0659503408
barkstone <barkstone2@gmail.com>
2024-04-25 13:44:55 +0900

    chore: 기본 유저 서비스 제거
    - 구글 ID Token 처리 라이브러리 취약점으로 인해 버전업

--- dailyquest_web/src/main/java/dailyquest/DailyquestApplication.java
+++ dailyquest_web/src/main/java/dailyquest/DailyquestApplication.java
@@ -3,8 +3,9 @@ package dailyquest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class DailyquestApplication {
 
 	public static void main(String[] args) {

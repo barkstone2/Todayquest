8b38ef891d77e8372733fe1d8869b0a3b85967b7
barkstone <barkstone2@gmail.com>
2024-04-27 15:59:59 +0900

    fix: 배치 컨트롤러 매핑 주소 오타 수정

--- dailyquest_batch/src/main/kotlin/dailyquest/batch/controller/BatchController.kt
+++ dailyquest_batch/src/main/kotlin/dailyquest/batch/controller/BatchController.kt
@@ -1,12 +1,9 @@
 package dailyquest.batch.controller
 
 import dailyquest.batch.service.JobService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RequestMapping("/ap1/v1")
+@RequestMapping("/api/v1")
 @RestController
 class BatchController(
     private val jobService: JobService,

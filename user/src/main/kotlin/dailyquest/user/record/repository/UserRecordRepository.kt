d6a5f07f6fa63e8789f43fc1805b100915083d1d
barkstone2 <68949845+barkstone2@users.noreply.github.com>
2024-06-03 17:48:56 +0900

    Revert "feat: 유저 기록 엔티티 삽입 API 추가"
    This reverts commit b271df47d6365097dd8cce2cad1beff21e89e7ae.

--- user/src/main/kotlin/dailyquest/user/record/repository/UserRecordRepository.kt
+++
@@ -1,6 +0,0 @@
-package dailyquest.user.record.repository
-
-import dailyquest.user.record.entity.UserRecord
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRecordRepository : JpaRepository<UserRecord, Long>

29f965eb8ee4384ea7c6c1a220dbbff3135df991
barkstone <barkstone2@gmail.com>
2024-06-03 15:33:35 +0900

    feat: 유저 레코드 엔티티를 사용하도록 변경
    - 유저 엔티티에서 레코드 필드 제거
    - 유저 DTO에서 레코드 값 제거
    - 유저 서비스의 레코드 처리 로직 제거
    - 퀘스트 커맨드 서비스에서 유저 레코드 서비스 사용하도록 변경
    - 테스트 코드 수정

--- user/src/main/kotlin/dailyquest/user/dto/UserResponse.kt
+++ user/src/main/kotlin/dailyquest/user/dto/UserResponse.kt
@@ -3,7 +3,6 @@ package dailyquest.user.dto
 import dailyquest.user.entity.ProviderType
 import dailyquest.user.entity.RoleType
 import dailyquest.user.entity.User
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
@@ -16,17 +15,6 @@ class UserResponse @JvmOverloads constructor(
     val exp: Long = 0,
     val gold: Long = 0,
     val role: RoleType = RoleType.USER,
-    val questRegistrationCount: Long = 0,
-    val questCompletionCount: Long = 0,
-    val currentQuestContinuousRegistrationDays: Long = 0,
-    val currentQuestContinuousCompletionDays: Long = 0,
-    val maxQuestContinuousRegistrationDays: Long = 0,
-    val maxQuestContinuousCompletionDays: Long = 0,
-    val lastQuestRegistrationDate: LocalDate? = null,
-    val lastQuestCompletionDate: LocalDate? = null,
-    val perfectDayCount: Long = 0,
-    val goldEarnAmount: Long = 0,
-    val goldUseAmount: Long = 0,
 ) {
     fun calculateLevel(expTable: Map<Int, Long>): Triple<Int, Long, Long> {
         var level = 1
@@ -61,17 +49,6 @@ class UserResponse @JvmOverloads constructor(
                  user.exp,
                  user.gold,
                  user.role,
-                 user.questRegistrationCount,
-                 user.questCompletionCount,
-                 user.currentQuestContinuousRegistrationDays,
-                 user.currentQuestContinuousCompletionDays,
-                 user.maxQuestContinuousRegistrationDays,
-                 user.maxQuestContinuousCompletionDays,
-                 user.lastQuestRegistrationDate,
-                 user.lastQuestCompletionDate,
-                 user.perfectDayCount,
-                 user.goldEarnAmount,
-                 user.goldUseAmount,
             )
         }
     }

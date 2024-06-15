ed387a16a086b09454828d346ee1f5d68435cb7d
barkstone <barkstone2@gmail.com>
2024-06-15 13:10:29 +0900

    chore: 웹 모듈 배포 스크립트 검증 로직 개선 및 가독성 증가

--- web/deploy/application-start.sh
+++ web/deploy/application-start.sh
@@ -7,46 +7,83 @@ COLOR=blue
 NEW_COLOR=green
 PORT=8181
 
-IS_BLUE_UP=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME-$COLOR)
+resolve_new_color() {
+    local is_blue_up=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME-$COLOR)
+    if [ "$is_blue_up" != "true" ]; then
+        NEW_COLOR=blue
+        COLOR=green
+        PORT=8080
+    fi
+}
 
-docker pull $REPOSITORY:$VERSION
+replace_docker_image() {
+    docker pull $REPOSITORY:$VERSION
+    docker tag $REPOSITORY:$VERSION $CONTAINER_NAME:$NEW_COLOR
+    docker rmi $REPOSITORY:$VERSION
+}
 
-if [ "$IS_BLUE_UP" != "true" ]; then
-    NEW_COLOR=blue
-    COLOR=green
-    PORT=8080
-fi
+start_new_container() {
+    /home/ec2-user/app/server/container-start.sh $CONTAINER_NAME-$NEW_COLOR $CONTAINER_NAME $NEW_COLOR $PORT
+}
 
-docker tag $REPOSITORY:$VERSION $CONTAINER_NAME:$NEW_COLOR
-docker rmi $REPOSITORY:$VERSION
+remove_new_container_and_image() {
+    docker stop $CONTAINER_NAME-$NEW_COLOR
+    docker rm $CONTAINER_NAME-$NEW_COLOR
+    docker rmi $CONTAINER_NAME:$NEW_COLOR
+}
 
-/home/ec2-user/app/server/container-start.sh $CONTAINER_NAME-$NEW_COLOR $CONTAINER_NAME $NEW_COLOR $PORT
+validate_new_container() {
+    sleep 30
+    local timeout=180
+    local interval=5
+    local elapsed=0
+    local is_new_up=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME-$NEW_COLOR)
+    if [ "$is_new_up" == "true" ]; then
+        while [ $elapsed -lt $timeout ]; do
+            docker logs $CONTAINER_NAME-$NEW_COLOR | grep 'Started DailyquestApplication'
+            is_new_init=$?
+            if [ $is_new_init -eq 0 ]; then
+                sleep 30
+                break
+            fi
+            sleep $interval
+            elapsed=$((elapsed + interval))
+        done
+    fi
 
-sleep 10
+    is_new_up=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME-$NEW_COLOR)
+    if [ "$is_new_up" != "true" ]; then
+        echo "New container initialization failed."
+        remove_new_container_and_image
+        exit 1
+    fi
+}
 
-IS_NEW_UP=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME-$NEW_COLOR)
-if [ "$IS_NEW_UP" == "true" ]; then
-    docker logs $CONTAINER_NAME-$NEW_COLOR | grep 'Completed initialization'
-    NEW_CONTAINER_INIT=$?
-    while [ "$NEW_CONTAINER_INIT" == 0 ]
-    do
-        sleep 1
-        docker logs $CONTAINER_NAME-$NEW_COLOR | grep 'Completed initialization'
-        NEW_CONTAINER_INIT=$?
-    done
-
+replace_nginx_config() {
     cp $NGINX_PATH/nginx-$NEW_COLOR.conf $NGINX_PATH/nginx.conf
     docker exec dailyquest-client nginx -s reload
+}
 
-    REMOVE_IMAGE=$(docker images -f "reference=$CONTAINER_NAME:$COLOR" -q)
-
+remove_previous_container() {
     # 컨테이너가 존재하지 않아도 스크립트가 중단되지 않음
     docker stop $CONTAINER_NAME-$COLOR
     docker rm $CONTAINER_NAME-$COLOR
+}
 
+remove_previous_image() {
     # 이미지가 존재하지 않으면 스크립트 중단됨
     # 기존 이미지가 존재할 때만 제거
-    if [ -n "$REMOVE_IMAGE" ]; then
-        docker rmi $REMOVE_IMAGE
+    local previous_image_exist=$(docker images -f "reference=$CONTAINER_NAME:$COLOR" -q)
+    if [ -n "$previous_image_exist" ]; then
+        docker rmi $CONTAINER_NAME:$COLOR
     fi
-fi
+}
+
+# 함수 호출
+resolve_new_color
+replace_docker_image
+start_new_container
+validate_new_container
+replace_nginx_config
+remove_previous_container
+remove_previous_image

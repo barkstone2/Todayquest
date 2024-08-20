af67f54dc303f09e8d7cfaf033f5076f97aa29a6
barkstone <barkstone2@gmail.com>
2024-08-20 13:00:52 +0900

    refactor: add user id condition to pref quest join query

--- web/src/main/java/dailyquest/preferencequest/repository/PreferenceQuestRepository.kt
+++ web/src/main/java/dailyquest/preferencequest/repository/PreferenceQuestRepository.kt
@@ -13,6 +13,6 @@ interface PreferenceQuestRepository : JpaRepository<PreferenceQuest, Long> {
     @Query("select pq from PreferenceQuest pq where pq.user.id = :userId and pq.deletedDate is null")
     fun getActivePrefQuests(@Param("userId") userId: Long): List<PreferenceQuest>
 
-    @Query("select count(q) from PreferenceQuest pq left join Quest q on q.preferenceQuest.id = pq.id where pq.user.id = :userId and pq.deletedDate is null group by pq.id")
+    @Query("select count(q) from PreferenceQuest pq left join Quest q on q.userId = pq.user.id and q.preferenceQuest.id = pq.id where pq.user.id = :userId and pq.deletedDate is null group by pq.id")
     fun getUsedCountOfActivePrefQuests(@Param("userId") userId: Long): List<Long>
 }

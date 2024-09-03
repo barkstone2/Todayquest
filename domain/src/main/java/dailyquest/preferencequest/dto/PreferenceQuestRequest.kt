62256f52e4763b151878863a08339d7a7d173565
barkstone <barkstone2@gmail.com>
2024-09-03 18:30:25 +0900

    feat: Change association of preference quest to indirect

--- domain/src/main/java/dailyquest/preferencequest/dto/PreferenceQuestRequest.kt
+++ domain/src/main/java/dailyquest/preferencequest/dto/PreferenceQuestRequest.kt
@@ -1,13 +1,12 @@
 package dailyquest.preferencequest.dto
 
 import dailyquest.preferencequest.entity.PreferenceQuest
-import dailyquest.user.entity.User
 
 interface PreferenceQuestRequest {
     val title: String
     val description: String
     val details: List<PreferenceDetailRequest>
-    fun mapToEntity(user: User): PreferenceQuest {
-        return PreferenceQuest.of(this.title, this.description, this.details.map { it.mapToEntity() }, user)
+    fun mapToEntity(userId: Long): PreferenceQuest {
+        return PreferenceQuest.of(this.title, this.description, this.details.map { it.mapToEntity() }, userId)
     }
 }
